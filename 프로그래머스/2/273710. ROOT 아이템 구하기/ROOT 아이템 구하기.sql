38bb1b84e0631c9bb85755207ff32862fcb34eb0
ssuzyn <88701965+ssuzyn@users.noreply.github.com>
2024-02-23 19:42:12 +0900

    [level 2] Title: ROOT 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
+++ 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
@@ -1,4 +1,5 @@
 -- 코드를 작성해주세요
-SELECT ITEM_ID, ITEM_NAME
-FROM ITEM_INFO
-WHERE ITEM_ID IN (SELECT ITEM_ID FROM ITEM_TREE WHERE PARENT_ITEM_ID IS NULL);
+select i.item_id, i.item_name
+from item_info i, item_tree t
+where t.parent_item_id is null and t.item_id=i.item_id
+order by i.item_id;
