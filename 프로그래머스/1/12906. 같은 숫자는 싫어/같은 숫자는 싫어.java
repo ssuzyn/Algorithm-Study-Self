c5fd6d8ff6a9db6a27c0a2ceee9368f7d6864660
ssuzyn <88701965+ssuzyn@users.noreply.github.com>
2024-03-28 18:32:35 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 45.75 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        
+        Stack<Integer> stack = new Stack<>();
+
+        for(int num : arr){
+        	if(!stack.empty() && stack.peek() == num) continue;
+            stack.push(num);
+        }
+        
+        int[] answer = new int[stack.size()];
+        for(int i = 0; i < stack.size(); i++){
+            answer[i] = stack.get(i);
+        }
+        return answer;
+    }
+}
