65b8ec22f500aa5c603499905f3ef5ff737f9041
ssuzyn <88701965+ssuzyn@users.noreply.github.com>
2024-03-29 11:19:12 +0900

    [level 2] Title: 기능개발, Time: 0.30 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,31 +3,22 @@ import java.util.*;
 class Solution {
     public List<Integer> solution(int[] progresses, int[] speeds) {
         
-        List<Integer> complete = new LinkedList<>();
+        Queue<Integer> q = new LinkedList<>();
     	List<Integer> answer = new ArrayList<>();
     	
     	for(int i = 0; i < progresses.length; i++) {
-    		int remain = 100 - progresses[i];
-    		int day = remain / speeds[i];
     		
-    		if(remain % speeds[i] != 0) day += 1;
-    		complete.add(day);
+    		double remain = (100 - progresses[i]) / (double)speeds[i];
+    		int date = (int) Math.ceil(remain);
+    		
+    		if(!q.isEmpty() && q.peek() < date) {
+    			answer.add(q.size());
+    			q.clear();
+    		}
+    		q.offer(date);
     	}
     	
-    	int count = 1;
-    	int day = complete.get(0);
-    	for(int i = 1; i < complete.size(); i++) {
-    		
-    		if(complete.get(i) <= day) {
-    			count++;
-    		}
-    		else {
-    			day = complete.get(i);
-        		answer.add(count);
-    			count = 1;
-    		}
-    	}
-        answer.add(count);
+    	answer.add(q.size());
     	
         return answer;
     }
