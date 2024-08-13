8e8419cd7fed158ef9f3f034f602c2040ef2e10c
ssuzyn <88701965+ssuzyn@users.noreply.github.com>
2024-08-13 21:04:00 +0900

    [D2] Title: 달팽이 숫자, Time: 214 ms, Memory: 21,276 KB -BaekjoonHub

---
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    // 달팽이 회전 방향: 우 -> 하 -> 좌 -> 상
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+        for (int t = 1; t <= test; t++) {
+            int N = sc.nextInt();
+            int[][] snail = new int[N][N];
+
+            int value = 1;
+            int x = 0, y = 0;
+            int idx = 0;
+
+            while(value <= N * N){
+                snail[x][y] = value++;
+                int nx = x + dx[idx];
+                int ny = y + dy[idx];
+
+                // 방향 재설정 = 좌표의 경계를 벗어나거나, 숫자가 이미 존재하는 경우
+                if(nx < 0 || nx >= N || ny < 0 || ny >= N || snail[nx][ny] != 0){
+                    idx = (idx + 1) % 4;
+                    nx = x + dx[idx];
+                    ny = y + dy[idx];
+                }
+                x = nx;
+                y = ny;
+            }
+
+            System.out.println("#" + t);
+            for(int i = 0; i < N; i++){
+                for(int j = 0; j < N; j++){
+                    System.out.print(snail[i][j] + " ");
+                }
+                System.out.println();
+            }
+
+        }
+    }
+}
