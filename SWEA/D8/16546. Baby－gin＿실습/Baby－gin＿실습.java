c057d90c6370626860c6fffefb092308ac32ae3c
ssuzyn <88701965+ssuzyn@users.noreply.github.com>
2024-08-13 20:09:20 +0900

    [D8] Title: Baby-gin_실습, Time: 107 ms, Memory: 18,388 KB -BaekjoonHub

--- SWEA/D8/16546. Baby－gin＿실습/Baby－gin＿실습.java
+++ SWEA/D8/16546. Baby－gin＿실습/Baby－gin＿실습.java
@@ -4,72 +4,63 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Solution {
-	
-	static int numbers[] = new int[6];
-	static boolean babygin = false;
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		
-		
-		for(int t = 1; t <= T; t++) {
-			String str = br.readLine();
-			for(int i = 0; i < 6; i++) {
-				numbers[i] = str.charAt(i) - '0';
-			}
-			
-			permutation(0);
-			System.out.println("#" + t + " " + babygin);
-			babygin = false;
-		
-		}
-	}
-	
-	private static void swap(int i, int j) {
-		int tmp = numbers[i];
-		numbers[i] = numbers[j];
-		numbers[j] = tmp;
-	}
-	
-	public static void permutation(int idx) {
-		if(babygin) {
-			return;
-		}
-		
-		if(idx == numbers.length) {
-			
-			int[] front = Arrays.copyOfRange(numbers, 0, 3);
-			int[] back = Arrays.copyOfRange(numbers, 3, 6);
-			
-			boolean frontRun = checkRun(front);
-			boolean frontTriplet = checkTriplet(front);
-			boolean backRun = checkRun(back);
-			boolean backTriplet = checkTriplet(back);
-			
-			babygin = (frontRun && backRun) || (frontRun && backTriplet) || 
-	                  (frontTriplet && backRun) || (frontTriplet && backTriplet);
-		
-//			System.out.println(frontRun + " " + frontTriplet + " " + backRun + " " + backTriplet);
-//			System.out.println(Arrays.toString(numbers) + " " + babygin + "\n");
 
-			return;
-		}
-		
-		for(int i = idx; i < numbers.length; i++) {
-			swap(idx, i);
-			permutation(idx + 1);
-			swap(idx, i);
-		}
-	}
-	
-	public static boolean checkRun(int[] arr) {
-		Arrays.sort(arr);
-		return arr[1] == arr[0] + 1 && arr[2] == arr[1] + 1;
-	}
-	
-	public static boolean checkTriplet(int[] arr) {
-		return arr[0] == arr[1] && arr[1] == arr[2] && arr[0] == arr[2];
-	}
-	
+    static int numbers[] = new int[6];
+    static boolean babygin = false;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for(int t = 1; t <= T; t++) {
+            String str = br.readLine();
+            for(int i = 0; i < 6; i++) {
+                numbers[i] = str.charAt(i) - '0';
+            }
+
+            permutation(0);
+            System.out.println("#" + t + " " + babygin);
+            babygin = false;
+
+        }
+    }
+
+    public static void permutation(int idx) {
+        if(babygin) {
+            return;
+        }
+
+        if(idx == numbers.length) {
+            int[] front = Arrays.copyOfRange(numbers, 0, 3);
+            int[] back = Arrays.copyOfRange(numbers, 3, 6);
+            babygin = checkBabyGin(front) && checkBabyGin(back);
+            return;
+        }
+
+        for(int i = idx; i < numbers.length; i++) {
+            swap(idx, i);
+            permutation(idx + 1);
+            swap(idx, i);
+        }
+    }
+
+    private static void swap(int i, int j) {
+        int tmp = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = tmp;
+    }
+
+    public static boolean checkBabyGin(int[] arr) {
+        Arrays.sort(arr);
+
+        if(arr[1] == arr[0] + 1 && arr[2] == arr[1] + 1) // check run
+            return true;
+
+        if(arr[0] == arr[1] && arr[1] == arr[2]) // check triplet
+            return true;
+
+        return false;
+    }
+
 }
+
