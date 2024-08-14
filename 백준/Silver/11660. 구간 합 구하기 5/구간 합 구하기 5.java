8debfbb42f0dfcf47c9bef0de0b319bd70f031f3
ssuzyn <88701965+ssuzyn@users.noreply.github.com>
2024-08-14 13:40:30 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1276 ms, Memory: 124408 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -4,37 +4,31 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
 		
-		int[][] sum = new int[N+1][N+1];
-		int[][] matrix = new int[N+1][N+1];
+		int N = Integer.parseInt(st.nextToken()); // 표의 크기
+		int M = Integer.parseInt(st.nextToken()); // 합 구하기 횟수
 		
+		int[][]map = new int[N+1][N+1];
 		
-		for(int i = 1; i < N+1; i++) {
+		for(int i = 1; i <= N; i++) { // 표 입력 받기
 			st = new StringTokenizer(br.readLine());
-			
-			for(int j = 1; j < N+1; j++) {
-				matrix[i][j] = Integer.parseInt(st.nextToken());
-				sum[i][j] = sum[i][j-1] + sum[i-1][j] - sum[i-1][j-1] + matrix[i][j];
+			for(int j = 1; j <= N; j++) {
+				int tmp = Integer.parseInt(st.nextToken());
+				map[i][j] = tmp + map[i-1][j] + map[i][j-1] - map[i-1][j-1];
 			}
 		}
 		
-		while(M-- > 0) {
+		for(int i = 0; i < M; i++) { // 구해야 하는 합 좌표 받기
 			st = new StringTokenizer(br.readLine());
 			int x1 = Integer.parseInt(st.nextToken());
 			int y1 = Integer.parseInt(st.nextToken());
 			int x2 = Integer.parseInt(st.nextToken());
 			int y2 = Integer.parseInt(st.nextToken());
-			
-			int answer = sum[x2][y2] - sum[x1-1][y2] - sum[x2][y1-1] + sum[x1-1][y1-1];
-			
-			System.out.println(answer);
+				
+			System.out.println(map[x2][y2] - map[x1-1][y2] - map[x2][y1-1] + map[x1-1][y1-1]);
 		}
 	}
-
 }
