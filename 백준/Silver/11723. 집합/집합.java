f9b4e54b760ea830508643b473f535599583eb5c
ssuzyn <88701965+ssuzyn@users.noreply.github.com>
2024-08-16 13:50:30 +0900

    [Silver V] Title: 집합, Time: 1120 ms, Memory: 307244 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,73 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int M = Integer.parseInt(br.readLine()); // 수행해야 하는 연산의 수
-		
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		int[] set = new int[21];
-		while(M-- > 0) {
-			String[] input = br.readLine().split(" ");
-			String op = input[0]; // 연산자
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int M = Integer.parseInt(br.readLine()); // 수행해야 하는 연산의 수
 
-			if(input.length > 1) {
-				int num = Integer.parseInt(input[1]); // 피연산자
-				
-				switch(op) {
-				case "add":
-					set[num] = 1;
-					break;
-					
-				case "check":
-					if((set[num] & 1) != 0) {
-						sb.append(1);
-					}
-					else {
-						sb.append(0);
-					}
+        StringBuilder sb = new StringBuilder();
+        int set = 0; // 비트마스크 사용
 
-					sb.append("\n");
-					break;
-					
-				case "remove":
-					set[num] = 0;
-					break;
-				
-				case "toggle":
-					if((set[num] & 1) != 0) {
-						set[num] = 0;
-					}
-					else {
-						set[num] = 1;
-					}
-					break;
-				}
-				
-			}
-			else {
-				switch(op) {
-				
-				case "all":
-					for(int i = 1; i <= 20; i++) {
-						set[i] = 1;
-					}
-					break;
-					
-				case "empty":
-					for(int i = 1; i <= 20; i++) {
-						set[i] = 0;
-					}
-					break;
-				}
-			}
-			
-		}
-		
-		System.out.println(sb.toString());
-	}
+        while (M-- > 0) {
+            String[] input = br.readLine().split(" ");
+            String op = input[0]; // 연산자
+
+            if (input.length > 1) {
+                int num = Integer.parseInt(input[1]); // 피연산자
+                int bit = 1 << num; // num번째 비트를 1로 설정
+
+                switch (op) {
+                    case "add":
+                        set |= bit; // num번째 비트를 1로 설정
+                        break;
+
+                    case "check":
+                        sb.append((set & bit) != 0 ? 1 : 0).append("\n");
+                        break;
+
+                    case "remove":
+                        set &= ~bit; // num번째 비트를 0으로 설정
+                        break;
+
+                    case "toggle":
+                        set ^= bit; // num번째 비트를 토글
+                        break;
+                }
+            } else {
+                switch (op) {
+                    case "all":
+                        set = (1 << 21) - 1; // 모든 비트를 1로 설정 (21비트 - 1)
+                        break;
+
+                    case "empty":
+                        set = 0; // 모든 비트를 0으로 설정
+                        break;
+                }
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
 }
