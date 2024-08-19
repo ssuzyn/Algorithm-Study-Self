1cafe00a3158cc52a7f356e73c734de35cd0a4cc
ssuzyn <88701965+ssuzyn@users.noreply.github.com>
2024-08-19 17:28:18 +0900

    [Unrated] Title: [모의 SW 역량테스트] 숫자 만들기, Time: 130 ms, Memory: 20,644 KB -BaekjoonHub

--- SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
+++ SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
@@ -3,18 +3,14 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Solution {
 
-	static int N;
-	static int[] input = new int[4]; // 입력받는 연산자의 갯수 정보
-	static int[] op; // 조합된 연산자
+	static int N, min, max;
+	static int[] op = new int[4]; // 입력받는 연산자의 갯수 정보
+	static int[] selected; // 조합된 연산자
 	static int[] numbers;
-	static List<Integer> answer;
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,15 +19,16 @@ public class Solution {
 		int T = Integer.parseInt(br.readLine());
 		
 		for(int t = 1; t <= T; t++) {
-			N = Integer.parseInt(br.readLine()); // 숫자 갯수
-			op = new int[N-1];
-			numbers = new int[N];
-			answer = new ArrayList<>();
+			min = Integer.MAX_VALUE;
+			max = Integer.MIN_VALUE;
 			
-			int opSize = 0;
+			N = Integer.parseInt(br.readLine()); // 숫자 갯수
+			selected = new int[N-1];
+			numbers = new int[N];
+			
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for(int i = 0; i < 4; i++) {
-				input[i] = Integer.parseInt(st.nextToken());
+				op[i] = Integer.parseInt(st.nextToken());
 			}
 			
 			st = new StringTokenizer(br.readLine());
@@ -40,51 +37,47 @@ public class Solution {
 			}
 			
 			permutation(0);
-			Collections.sort(answer); // 수식으로 얻은 결과값 오름차순 정렬
 			
-			
-			
-			bw.write("#" + t + " " + Math.abs(answer.get(0) - answer.get(answer.size()-1)) + "\n");
+			bw.write("#" + t + " " + (max - min) + "\n");
 		}
 		bw.close();
 	}
 	
 	private static void permutation(int idx) {
 		if(idx == N-1) {
-			cal();
+			int result = numbers[0]; // 첫번째 숫자 값 넣어주기
+			
+			for(int i = 0; i < N-1; i++) {
+				switch(selected[i]) {
+				case 0: // + 연산자
+					result += numbers[i+1];
+					break;
+				case 1: // - 연산자
+					result -= numbers[i+1];
+					break;
+				case 2: // X 연산자
+					result *= numbers[i+1];
+					break;
+				case 3: // / 연산자
+					result /= numbers[i+1];
+					break;
+				}
+			}
+			
+			// 최소값, 최대값 업데이트
+			max = Math.max(max, result);
+			min = Math.min(min, result);
 			return;
 		}
 		
+		
 		for(int i = 0; i < 4; i++) {
-			if(input[i] < 1) continue;
+			if(op[i] < 1) continue;
 			
-			op[idx] = i;
-			input[i]--;
+			selected[idx] = i;
+			op[i]--;
 			permutation(idx + 1);
-			input[i]++;
+			op[i]++;
 		}
-	}
-	
-	private static void cal() {
-		int result = numbers[0];
-		
-		for(int i = 0; i < N-1; i++) {
-			switch(op[i]) {
-			case 0: // + 연산자
-				result += numbers[i+1];
-				break;
-			case 1: // - 연산자
-				result -= numbers[i+1];
-				break;
-			case 2: // X 연산자
-				result *= numbers[i+1];
-				break;
-			case 3: // / 연산자
-				result /= numbers[i+1];
-				break;
-			}
-		}
-		
-		answer.add(result);
 	}
 }
