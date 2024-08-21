703ad43b649168163a2a5d8d82140942b8b99248
ssuzyn <88701965+ssuzyn@users.noreply.github.com>
2024-08-21 16:19:55 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 526 ms, Memory: 38,180 KB -BaekjoonHub

--- SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
+++ SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
@@ -8,6 +8,8 @@ public class Solution {
 
 	static int D, W, K;
 	static int[][] film;
+	static int[][] map;
+	static boolean[] isSelected;
 	static int drugCnt;
 	
 	public static void main(String[] args) throws IOException{
@@ -22,55 +24,69 @@ public class Solution {
 
 			drugCnt = Integer.MAX_VALUE;
 			film = new int[D][W];
+			map = new int[D][W];
+			isSelected = new boolean[D];
+			
 			for(int i = 0; i < D; i++) {
 				st = new StringTokenizer(br.readLine());
 				for(int j = 0; j < W; j++) {
 					film[i][j] = Integer.parseInt(st.nextToken());
+					map[i][j] = film[i][j];
 				}
 			}
 			
 
-			drug(0, 0);
-			System.out.println("#" + t + " " + drugCnt);
+			if(test()) {
+				System.out.println("#" + t + " " + 0);
+			}
+			else {
+				subSet(0);
+				System.out.println("#" + t + " " + drugCnt);
+			}
 			
 		}
 	}
 	
-	// idx: 약물투여 대상 행의 위치, cnt: 약물 투여 횟수
-	private static void drug(int idx, int cnt) {
-		
-		if(test()) { // 검사에 통과한 경우, 최소 투여 횟수로 설정
-			drugCnt = Math.min(cnt, drugCnt);
+	// 부분 집합으로 투여할 행 선택하기
+	private static void subSet(int idx) {
+		if(idx == D) {
+			drug(isSelected, 0, 0);
+			reset();
 			return;
 		}
 		
-		if(cnt > drugCnt) return; // 최소 투여 횟수보다 많은 경우 return
+
+		subSet(idx + 1);
 		
-		if(idx == D) return; // 모든 행을 탐색한 경우 return
-			
+		isSelected[idx] = true;
+		subSet(idx + 1);
+		isSelected[idx] = false;
+	}
+	
+	// idx: 약물투여 대상 행의 위치, cnt: 약물 투여 횟수
+	private static void drug(boolean[] isSelected, int idx, int cnt) {
 		
-		int[] memory = new int[W];
+		if(cnt >= drugCnt) return;
 		
-		// 약물 투여하지 않고 검사하기
-		for(int i = 0; i < W; i++) {
-			memory[i] = film[idx][i];
+		if(idx == D) {
+			if(test()) {
+				drugCnt = Math.min(cnt, drugCnt);
+			}
+			return;
 		}
-		drug(idx + 1, cnt);
 		
-		// idx행 약품 A 투여하고 검사하기
-		for(int i = 0; i < W; i++) {
-			film[idx][i] = 0;
+		if(isSelected[idx]) {
+
+			Arrays.fill(film[idx], 1);
+			drug(isSelected, idx + 1, cnt + 1);
+				
+			Arrays.fill(film[idx], 0);
+			drug(isSelected, idx + 1, cnt + 1);
 		}
-		drug(idx + 1, cnt + 1);
-		
-		// idx행 약품 B 투여하고 검사하기
-		for(int i = 0; i < W; i++) {
-			film[idx][i] = 1;
+		else {
+			drug(isSelected, idx + 1, cnt);
 		}
-		drug(idx + 1, cnt + 1);
 		
-		// idx행 원상복구
-		film[idx] = Arrays.copyOf(memory, memory.length);
  	}
 	
 	
@@ -98,5 +114,14 @@ public class Solution {
 		return true;
 		
 	}
-
+	
+	// 보호필름 초기화 하기
+	private static void reset() {
+		for(int i = 0; i < D; i++) {
+			for(int j = 0; j < W; j++) {
+				film[i][j] = map[i][j];
+			}
+		}
+	}
+	
 }
