1c7ec5149b32689aed818daea1e66e468cce7e18
ssuzyn <88701965+ssuzyn@users.noreply.github.com>
2024-10-02 20:05:11 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 196 ms, Memory: 16516 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
@@ -19,39 +19,31 @@ public class Main {
             egg[i][0] = Integer.parseInt(st.nextToken()); // 내구도
             egg[i][1] = Integer.parseInt(st.nextToken()); // 무게
         }
-        solve(0);
+        solve(0, 0);
         System.out.println(answer);
     }
 
-    private static void solve(int turn) {
-        if(turn == N){
-            int cnt = 0;
-            for(int i = 0; i < N; i++){
-                if(egg[i][0] <= 0) cnt++;
-            }
+    private static void solve(int turn, int cnt) {
+        if(cnt == N-1 || turn == N){
             answer = Math.max(cnt, answer);
             return;
         }
 
         if(egg[turn][0] <= 0) { // 현재 들고 있는 계란이 깨진 경우
-            solve(turn + 1);
-            return;
+            solve(turn + 1, cnt);
         }
+        else{
+            for(int i = 0; i < N; i++){
+                if (i == turn || egg[i][0] <= 0) continue; // 자신이나 깨진 계란 제외
 
-        boolean brokenEgg = false;
-        for(int i = 0; i < N; i++){
-            if (i == turn || egg[i][0] <= 0) continue; // 자신이나 깨진 계란 제외
+                egg[turn][0] -= egg[i][1];
+                egg[i][0] -= egg[turn][1];
 
-            brokenEgg = true;
-            egg[turn][0] -= egg[i][1];
-            egg[i][0] -= egg[turn][1];
+                solve(turn + 1, cnt + (egg[turn][0] <= 0? 1 : 0) + (egg[i][0] <= 0? 1 : 0));
 
-            solve(turn + 1);
-
-            egg[turn][0] += egg[i][1];
-            egg[i][0] += egg[turn][1];
+                egg[turn][0] += egg[i][1];
+                egg[i][0] += egg[turn][1];
+            }
         }
-        if(!brokenEgg) // 깨지지 않은 다른 계란이 없을 경우
-            solve(turn + 1);
     }
 }
