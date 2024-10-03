b69468c1978db8810a50efea69769d54cb86299e
ssuzyn <88701965+ssuzyn@users.noreply.github.com>
2024-10-03 17:03:29 +0900

    [D3] Title: 상호의 배틀필드, Time: 188 ms, Memory: 35,360 KB -BaekjoonHub

--- SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
+++ SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
@@ -1,118 +1,116 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Solution {
 
-	static int H, W;
-	static char[][] map;
-	static int[] tank = new int[3]; // [0]: 행 위치, [1]: 열 위치, [2]: 방향 인덱스 (0: '^', 1: 'v', 2: '<', 3: '>')
-	static int[][] d = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // 상, 하, 좌, 우
-	static char[] direction = {'^', 'v', '<', '>'}; // 방향에 따른 전차 모양
+    static int H, W, N;
+    static char[][] map;
+    static int[][] d = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // 상, 하, 좌, 우
+    static char[] direction = {'^', 'v', '<', '>'}; // 방향에 따른 전차 모양
+    static Tank tank;
 
-	public static void main(String[] args) throws IOException {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    int T = Integer.parseInt(br.readLine()); // 테스트 케이스
+    static class Tank{
+        int x, y;
+        int status;
 
-	    for (int t = 1; t <= T; t++) {
-	        String[] input = br.readLine().split(" ");
-	        H = Integer.parseInt(input[0]); // 높이
-	        W = Integer.parseInt(input[1]); // 너비
-	        map = new char[H][W];
+        Tank(int x, int y, int status){
+            this.x = x;
+            this.y = y;
+            this.status = status;
+        }
+    }
 
-	        for (int i = 0; i < H; i++) {
-	            String line = br.readLine();
-	            map[i] = line.toCharArray();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
 
-	            for (int j = 0; j < W; j++) {
-	                char c = map[i][j];
-	                if (c == '^' || c == 'v' || c == '<' || c == '>') {
-                        tank[0] = i; // 행 위치
-                        tank[1] = j; // 열 위치
-                         
-                        // 방향 인덱스를 저장
-                        if (c == '^') tank[2] = 0;
-                        if (c == 'v') tank[2] = 1;
-                        if (c == '<') tank[2] = 2;
-                        if (c == '>') tank[2] = 3;
-                    }
-	            }
-	        }
+        for(int t = 1; t <= T; t++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            H = Integer.parseInt(st.nextToken());
+            W = Integer.parseInt(st.nextToken());
+            map = new char[H][W];
 
-	        int size = Integer.parseInt(br.readLine());
-	        char[] user = br.readLine().toCharArray();
+            for(int h = 0; h < H; h++){
+                map[h] = br.readLine().toCharArray();
+                for(int w = 0; w < W; w++){
+                    if(map[h][w] == '^') tank = new Tank(h, w, 0);
+                    else if(map[h][w] == 'v') tank = new Tank(h, w, 1);
+                    else if(map[h][w] == '<') tank = new Tank(h, w, 2);
+                    else if(map[h][w] == '>') tank = new Tank(h, w, 3);
+                }
+            }
 
-	        for (char cmd : user) {
-	            game(cmd);
-	        }
+            N = Integer.parseInt(br.readLine());
+            for(char cmd : br.readLine().toCharArray()){
+                battle(cmd);
+            }
 
-	        System.out.print("#" + t + " ");
-	        print();
-	    }
-	}
+            System.out.print("#" + t + " ");
+            for(int i = 0; i < map.length; i++){
+                for(int j = 0; j < map[i].length; j++){
+                    System.out.print(map[i][j]);
+                }
+                System.out.println();
+            }
+        }
+    }
 
-	private static void print() {
-	    for (int i = 0; i < H; i++) {
-	        for (int j = 0; j < W; j++) {
-	            System.out.print(map[i][j]);
-	        }
-	        System.out.println();
-	    }
-	}
+    private static boolean isValid(int x, int y) {
+        return x >= 0 && x < H && y >= 0 && y < W;
+    }
 
-	private static boolean isValid(int x, int y) {
-	    return x >= 0 && x < H && y >= 0 && y < W;
-	}
+    private static void move(int status){ // status : 바꿀 방향
+        tank.status = status;
+        map[tank.x][tank.y] = direction[status];
+        int nx = tank.x + d[tank.status][0];
+        int ny = tank.y + d[tank.status][1];
 
-	private static void moveTank(int dir) {
-	    tank[2] = dir; // 전차 방향 변경
-	    map[tank[0]][tank[1]] = direction[dir];
+        if(isValid(nx, ny) && map[nx][ny] == '.') {
+            map[nx][ny] = map[tank.x][tank.y];
+            map[tank.x][tank.y] = '.';
+            tank.x = nx;
+            tank.y = ny;
+        }
 
-	    int nx = tank[0] + d[dir][0];
-	    int ny = tank[1] + d[dir][1];
+    }
 
-	    if (isValid(nx, ny) && map[nx][ny] == '.') { // 다음 이동할 위치가 평지인 경우
-	        map[nx][ny] = map[tank[0]][tank[1]]; // 다음 전차가 이동하는 위치
-	        map[tank[0]][tank[1]] = '.'; // 현재 전차가 있는 위치 -> 평지
-	        tank[0] = nx; // 전차 위치 변경
-	        tank[1] = ny;
-	    }
-	}
+    private static void fire(){
+        int nx = tank.x;
+        int ny = tank.y;
 
-	private static void fire() {
-	    int x = tank[0], y = tank[1];
-	    int dir = tank[2];
+        while(true){
+            nx += d[tank.status][0];
+            ny += d[tank.status][1];
 
-	    while (true) {
-	        x += d[dir][0];
-	        y += d[dir][1];
+            if(!isValid(nx, ny) || map[nx][ny] == '#') break;
 
-	        if (!isValid(x, y)) break; // 맵 밖으로 나가면 중지
-	        if (map[x][y] == '#') break; // 강철벽을 만나면 중지
-	        if (map[x][y] == '*') { // 벽돌벽을 만나면 파괴 후 중지
-	            map[x][y] = '.';
-	            break;
-	        }
-	    }
-	}
+            if(map[nx][ny] == '*') {
+                map[nx][ny] = '.';
+                break;
+            }
+        }
+    }
 
-	private static void game(char cmd) {
-	    switch (cmd) {
-	        case 'U':
-	            moveTank(0);
-	            break;
-	        case 'D':
-	            moveTank(1);
-	            break;
-	        case 'L':
-	            moveTank(2);
-	            break;
-	        case 'R':
-	            moveTank(3);
-	            break;
-	        case 'S':
-	            fire();
-	            break;
-	    }
-	}
+    private static void battle(char cmd){
+
+        switch(cmd) {
+            case 'U':
+                move(0);
+                break;
+            case 'D':
+                move(1);
+                break;
+            case 'L':
+                move(2);
+                break;
+            case 'R':
+                move(3);
+                break;
+            case 'S':
+                fire();
+                break;
+        }
+    }
 }
