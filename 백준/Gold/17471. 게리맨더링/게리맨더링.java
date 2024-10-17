882b288c29ed67c4e075e197ea6e08750bfab95f
ssuzyn <88701965+ssuzyn@users.noreply.github.com>
2024-10-18 00:52:57 +0900

    [Gold III] Title: 게리맨더링, Time: 128 ms, Memory: 15636 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -2,109 +2,94 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int N;
-	static int minDiff = Integer.MAX_VALUE;
-	static int people[];
-	static boolean isSelected[];
-	static List<Integer>[] matrix;
 
-	 public static void main(String[] args) throws IOException {
-	       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	       N = Integer.parseInt(br.readLine()); // 구역의 개수
-	       people = new int[N + 1]; // 각 구역의 인구 수 
-	       matrix = new ArrayList[N + 1]; // 인접 행렬
-	       isSelected = new boolean[N + 1];
-	       
-	       StringTokenizer st = new StringTokenizer(br.readLine());
-	       for(int i = 1; i <= N; i++) {
-	    	   people[i] = Integer.parseInt(st.nextToken());
-	       }
-	       
-	       for(int i = 1; i <= N; i++) {
-	    	   matrix[i] = new ArrayList<>();
-	       }
-	       
-	       for(int i = 1; i <= N; i++) {
-	    	   st = new StringTokenizer(br.readLine());
-	    	   int M = Integer.parseInt(st.nextToken()); // 해당 구역과 인접한 구역의 수
-	    	   while(M-- > 0) {
-	    		   int tmp = Integer.parseInt(st.nextToken());
-	    		   matrix[i].add(tmp);
-	    		   matrix[tmp].add(i);
-	    	   }
-	       }
-	       
-	       divide(1);
-	       
-	       if(minDiff == Integer.MAX_VALUE) {
-	    	   System.out.println(-1);
-	       }
-	       else {
-		       System.out.println(minDiff);
-	       }
-	       
-	       
-	 }
-	 
-	 private static void divide(int index) {
-		 
-		 if(index == N) { // 부분 집합 완성
-			 
-			 // 두 구역으로 나누기
-			 List<Integer> groupA = new ArrayList<>();
-			 List<Integer> groupB = new ArrayList<>();
-			 
-			 for(int i = 1; i <= N; i++) {
-				 if(isSelected[i]) groupA.add(i);
-				 else groupB.add(i);
-			 }
-			 
-			 // 두 그룹 모두에 구역이 있어야 함
-			 if(groupA.size() == 0 || groupB.size() == 0) return;
-			 
-			// 두 그룹이 각각 연결되어 있는지 확인
-	            if (isConnected(groupA) && isConnected(groupB)) {
-	                int sumA = 0, sumB = 0;
-	                for (int i : groupA) sumA += people[i];
-	                for (int i : groupB) sumB += people[i];
-	                minDiff = Math.min(minDiff, Math.abs(sumA - sumB));
-	            }
-			 
-			 return;
-		 }
-		 
-		 isSelected[index] = true;
-		 divide(index + 1);
-		 
-		 isSelected[index] = false;
-		 divide(index + 1);
-	 }
-	 
-	 private static boolean isConnected(List<Integer> group) {
-		 boolean[] visited = new boolean[N + 1];
-		 Queue<Integer> queue = new LinkedList<>();
-		 queue.add(group.get(0));
-		 visited[group.get(0)] = true;
-		 
-		 int count = 1;
-	        while (!queue.isEmpty()) {
-	            int current = queue.poll();
-	            for (int next : matrix[current]) {
-	                if (group.contains(next) && !visited[next]) {
-	                    visited[next] = true;
-	                    queue.add(next);
-	                    count++; // 인접한 구역인 경우 카운트
-	                }
-	            }
-	        }
-		 
-		 return count == group.size(); // 인접한 구역의 갯수 = 현재 선거구에 포함된 구역의 갯수
-	 }
+    static int N, answer = Integer.MAX_VALUE;
+    static int[] population;
+    static List[] section;
+    static boolean[] selected;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine()); // 구역의 개수
+        population = new int[N + 1];
+        section = new List[N + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            population[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= N; i++) {
+            section[i] = new ArrayList<>();
+        }
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int size = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < size; j++) {
+                section[i].add(Integer.parseInt(st.nextToken()));
+            }
+        }
+
+        divide(1, new boolean[N + 1]);
+        System.out.println(answer == Integer.MAX_VALUE ? -1 : answer);
+    }
+
+    private static void divide(int idx, boolean[] selected) {
+        if (idx == N) {
+            int diff = 0;
+            List<Integer> groupA = new ArrayList<>();
+            List<Integer> groupB = new ArrayList<>();
+            for (int i = 1; i <= N; i++) {
+                if (selected[i]) {
+                    groupA.add(i);
+                    diff += population[i];
+                } else {
+                    groupB.add(i);
+                    diff -= population[i];
+                }
+            }
+
+            if (groupA.isEmpty() || groupB.isEmpty()) {
+                return;
+            }
+
+            if (check(groupA) & check(groupB)) {
+                answer = Math.min(answer, Math.abs(diff));
+            }
+            return;
+
+        }
+
+        selected[idx] = true;
+        divide(idx + 1, selected);
+
+        selected[idx] = false;
+        divide(idx + 1, selected);
+    }
+
+    private static boolean check(List<Integer> group) {
+        boolean[] visited = new boolean[N + 1];
+        isConnect(group.get(0), group, visited);
+
+        for(int node : group){
+            if(!visited[node]) return false;
+        }
+        return true;
+    }
+
+    private static void isConnect(int idx, List<Integer> group, boolean[] visited) {
+        visited[idx] = true;
+
+        for (int j = 0; j < section[idx].size(); j++) {
+            int neighbor = (int) section[idx].get(j);
+            if (group.contains(neighbor) && !visited[neighbor]) {
+                isConnect(neighbor, group, visited);
+            }
+        }
+    }
 }
