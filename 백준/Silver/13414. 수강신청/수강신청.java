d5a34077f96a82b06766151463258269dd0fb2c8
ssuzyn <88701965+ssuzyn@users.noreply.github.com>
2024-10-31 00:22:08 +0900

    [Silver III] Title: 수강신청, Time: 1092 ms, Memory: 66888 KB -BaekjoonHub

--- 백준/Silver/13414. 수강신청/수강신청.java
+++ 백준/Silver/13414. 수강신청/수강신청.java
@@ -3,34 +3,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedHashSet;
 import java.util.Set;
-import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int K = Integer.parseInt(input[0]); // 과목의 수강 가능 인원
+        int L = Integer.parseInt(input[1]); // 대기 목록의 길이
 
-        int max = Integer.parseInt(st.nextToken());
-        int size = Integer.parseInt(st.nextToken());
-
-        Set<String> studentId = new LinkedHashSet<>();
-
-        for (int i = 0; i < size; i++) {
-            String id = br.readLine();
-            
-            if(studentId.contains(id)) {
-            	studentId.remove(id);
+        Set<String> set = new LinkedHashSet<>();
+        for(int i = 0; i < L; i++){
+            String student = br.readLine();
+            if(set.contains(student)){
+                set.remove(student);
             }
-            studentId.add(id);
+            set.add(student);
         }
 
-        int count = 0;
-        for (String id : studentId) {
-        	System.out.println(id);
-            count++;
-            if (count == max) break;
+        for(String s : set){
+            K--;
+            System.out.println(s);
+            if(K == 0) break;
         }
-        
-	}
+
+    }
 }
