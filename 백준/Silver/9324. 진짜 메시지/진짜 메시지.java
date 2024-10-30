9e3e7af463b01411133654b11b306cf80bf9202d
ssuzyn <88701965+ssuzyn@users.noreply.github.com>
2024-10-30 23:51:00 +0900

    [Silver IV] Title: 진짜 메시지, Time: 340 ms, Memory: 42540 KB -BaekjoonHub

--- 백준/Silver/9324. 진짜 메시지/진짜 메시지.java
+++ 백준/Silver/9324. 진짜 메시지/진짜 메시지.java
@@ -1,41 +1,47 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-	public static void main(String args[]) {
-		
-		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
-		scan.nextLine();
-	
-		for(int i = 0; i < n; i++) {
-			
-			int[] value = new int[26];
-			String msg = scan.nextLine();
-			
-			boolean isFake = false;
-			
-			for(int j = 0; j < msg.length(); j++) {
-				int index = msg.charAt(j) - 'A';
-				value[index]++;
-				
-				if(value[index] == 3) {
-					if(j == msg.length()-1 || msg.charAt(j+1) != msg.charAt(j)) {
-						isFake = true;
-					}
-					
-					value[index] = 0;
-					j++;
-				}
-				
-			}
-			
-			if(isFake)
-				System.out.println("FAKE");
-			else
-				System.out.println("OK");
-			
-		}
-	
-		
-	}
+
+    private static void print(int[] arr){
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < N; i++) {
+            char[] input = br.readLine().toCharArray();
+            int[] alphabet = new int[26];
+            boolean wrong = false;
+
+            for(int j = 0; j < input.length; j++){
+                char tmp = input[j];
+                alphabet[tmp - 'A']++;
+                if(alphabet[tmp - 'A'] == 3){
+                    if(j + 1 < input.length){
+                        if(input[j + 1] != tmp){
+                            wrong = true;
+                            break;
+                        }
+                        else{
+                            alphabet[tmp - 'A'] = 0;
+                            j++;
+                        }
+                    }
+                    else{
+                        wrong = true;
+                        break;
+                    }
+                }
+            }
+            if(wrong) sb.append("FAKE\n");
+            else sb.append("OK\n");
+        }
+        System.out.print(sb);
+    }
 }
