ddae7bbbda74be30ad14f2c7d191ef6fc0f1e0d7
ssuzyn <88701965+ssuzyn@users.noreply.github.com>
2024-10-31 13:41:16 +0900

    [Silver I] Title: 트럭, Time: 124 ms, Memory: 15220 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.java
+++ 백준/Silver/13335. 트럭/트럭.java
@@ -5,49 +5,51 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int n = Integer.parseInt(st.nextToken()); // 다리 건너는 트럭 
-        int w = Integer.parseInt(st.nextToken()); // 다리 길이
-        int L = Integer.parseInt(st.nextToken()); // 다리 최대 하중
-        
-        Queue<Integer> truck = new LinkedList<>();
-        Queue<Integer> bridge = new LinkedList<>();
-        
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i++) {
-        	truck.offer(Integer.parseInt(st.nextToken()));
-        }
-        
-        
-        for(int i = 0; i < w; i++) {
-        	bridge.offer(0);
-        }
-        
-        int bridgeWeight = 0;
-        int time = 0;
-        
-        while(!bridge.isEmpty()) {
-        	time++;
-        	bridgeWeight -= bridge.poll();
-        	
-        	if(!truck.isEmpty()) {
-        		if(truck.peek() + bridgeWeight <= L) {
-        			bridgeWeight += truck.peek();
-        			bridge.add(truck.poll());
-        		}
-        		else {
-        			bridge.offer(0);
-        		}
-        		
-        	}
-        }
-        
-        System.out.println(time);
-       
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken()); // 트럭의 개수
+		int w = Integer.parseInt(st.nextToken()); // 다리의 길이
+		int L = Integer.parseInt(st.nextToken()); // 다리의 최대 하중
+		
+		Queue<Integer> truck = new LinkedList<>();
+		Queue<Integer> bridge = new LinkedList<>();
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			truck.add(Integer.parseInt(st.nextToken()));
+		}
+		
+		for(int i = 0; i < w; i++) {
+			bridge.add(0);
+		}
+	
+		int time = 0;
+		int weight = 0;
+		while(!bridge.isEmpty()) {
+			time++;
+			weight -= bridge.poll();
+			
+			while(!truck.isEmpty()) {
+				int tmp = truck.peek();
+				
+				if(weight + tmp <= L) {
+					truck.poll();
+					bridge.add(tmp);
+					weight += tmp;
+					break;
+					
+				}
+				else {
+					bridge.add(0);
+					break;
+				}
+			}
+		}
+		
+		System.out.println(time);
 	}
 }
