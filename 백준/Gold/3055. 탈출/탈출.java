80a6a08d09dfb42a39153710d78919a05fda54ef
ssuzyn <88701965+ssuzyn@users.noreply.github.com>
2024-11-05 17:30:07 +0900

    [Gold IV] Title: 탈출, Time: 104 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.java
+++ 백준/Gold/3055. 탈출/탈출.java
@@ -7,94 +7,89 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int R, C;
-    static char[][] map;
-    static boolean[][] visited;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    static Queue<int[]> waterQ = new LinkedList<>();
-    static Queue<int[]> hedgehogQ = new LinkedList<>();
+	static int R, C;
+	static char[][] map;
+	static boolean[][] visited;
+	static int[][] d = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+	static Queue<int[]> water = new LinkedList<>();
+	static Queue<int[]> q = new LinkedList<>();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        R = Integer.parseInt(st.nextToken()); // 행
-        C = Integer.parseInt(st.nextToken()); // 열
-        map = new char[R][C];
-        visited = new boolean[R][C];  // 방문 체크 배열
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken()); // 세로
+		C = Integer.parseInt(st.nextToken()); // 가로
+		
+		map = new char[R][C];
+		visited = new boolean[R][C];
 
-        for(int i = 0; i < R; i++) {
-            map[i] = br.readLine().toCharArray();
-            for(int j = 0; j < C; j++) {
-                if(map[i][j] == 'S') {
-                    hedgehogQ.add(new int[]{i, j, 0});
-                    visited[i][j] = true;  // 고슴도치의 초기 위치 방문 처리
-                }
-                if(map[i][j] == '*') waterQ.add(new int[]{i, j});
-            }
-        }
+		
+		for (int i = 0; i < R; i++) {
+			map[i] = br.readLine().toCharArray();
+			for(int j = 0; j < C; j++) {
+				if(map[i][j] == '*') water.add(new int[] {i, j});
+				else if(map[i][j] == 'S') {
+					q.add(new int[] {i, j, 0});
+					map[i][j] = '.';
+					visited[i][j] = true;
+				}
+			}
+		}
 
-        int answer = bfs();
-        if(answer == -1) System.out.println("KAKTUS");
-        else System.out.println(answer);
+		int answer = bfs();
+		System.out.println(answer == -1? "KAKTUS" : answer);
+	}
+	
+	private static int bfs() {
+		
+		while(!q.isEmpty()) {
+			
+			waterBfs();
+			int size = q.size();
+			
+			for(int i = 0; i < size; i++) {
+				int[] cur = q.poll();
+				int x = cur[0];
+				int y = cur[1];
+				int depth = cur[2];
+				
+				if(map[x][y] == 'D') return depth;
+				
+				for(int[] dir : d) {
+					int nx = x + dir[0];
+					int ny = y + dir[1];
+					
+					if(nx < 0 || ny < 0 || nx >= R || ny >= C || visited[nx][ny]) continue;
+					if(map[nx][ny] == 'X' || map[nx][ny] == '*') continue;
+					
+					visited[nx][ny] = true;
+					q.add(new int[] {nx, ny, depth + 1});
+				}
+			}
+		}
 
-    }
-
-    private static boolean isValid(int x, int y) {
-        return x >= 0 && x < R && y >= 0 && y < C;
-    }
-
-    private static void flood(){
-        int size = waterQ.size();
-
-        for(int s = 0; s < size; s++){
-            int[] cur = waterQ.poll();
-            int x = cur[0];
-            int y = cur[1];
-
-            for(int d = 0; d < 4; d++){
-                int nx = x + dx[d];
-                int ny = y + dy[d];
-
-                if(!isValid(nx, ny) || map[nx][ny] == 'D' ||
-                    map[nx][ny] == '*' || map[nx][ny] == 'X') continue;
-
-                map[nx][ny] = '*';
-                waterQ.add(new int[]{nx, ny});
-            }
-        }
-    }
-
-    private static int bfs(){
-        while(!hedgehogQ.isEmpty()){
-
-            // 1. 물 확장
-            flood();
-
-            // 2. 고슴도치 이동
-            int size = hedgehogQ.size();
-
-            for(int s = 0; s < size; s++){
-                int[] cur = hedgehogQ.poll();
-                int x = cur[0];
-                int y = cur[1];
-                int depth = cur[2];
-
-                if(map[x][y] == 'D') return depth;  // 비버 굴에 도착하면 종료
-
-                for(int d = 0; d < 4; d++){
-                    int nx = x + dx[d];
-                    int ny = y + dy[d];
-
-                    if(!isValid(nx, ny) || visited[nx][ny] ||
-                        map[nx][ny] == '*' || map[nx][ny] == 'X') continue;
-
-                    hedgehogQ.add(new int[]{nx, ny, depth + 1});
-                    visited[nx][ny] = true;  // 방문 처리
-                }
-            }
-        }
-
-        return -1;
-    }
+		return -1;
+	}
+	
+	private static void waterBfs() {
+		int size = water.size();
+		
+		for(int i = 0; i < size; i++) {
+			int[] cur = water.poll();
+			int x = cur[0];
+			int y = cur[1];
+			
+			for(int[] dir : d) {
+				int nx = x + dir[0];
+				int ny = y + dir[1];
+				
+				if(nx < 0 || ny < 0 || nx >= R || ny >= C) continue;
+				if(map[nx][ny] == 'X' || map[nx][ny] == 'D' || map[nx][ny] == '*') continue;
+				
+				map[nx][ny] = '*';
+				water.add(new int[] {nx, ny});
+			}
+		}
+		
+	}
 }
