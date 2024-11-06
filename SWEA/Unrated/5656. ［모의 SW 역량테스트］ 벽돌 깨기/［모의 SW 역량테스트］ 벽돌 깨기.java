3d542fb147215f9f42d6ab5b585e3199dcc85729
ssuzyn <88701965+ssuzyn@users.noreply.github.com>
2024-11-06 23:15:44 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벽돌 깨기, Time: 1,147 ms, Memory: 111,724 KB -BaekjoonHub

--- SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
+++ SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
@@ -1,6 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Solution {
@@ -20,12 +23,12 @@ public class Solution {
 			N = Integer.parseInt(st.nextToken());
 			W = Integer.parseInt(st.nextToken());
 			H = Integer.parseInt(st.nextToken());
-			
+
 			origin = new int[H][W];
 			map = new int[H][W];
 			result = 0; // 최대로 제거할 수 있는 벽돌의 개수
 			totalOfBrick = 0; // 총 벽돌 개수
-			
+
 			for (int i = 0; i < H; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < W; j++) {
@@ -57,7 +60,7 @@ public class Solution {
 	private static void shooting(int[] turn) {
 		count = 0;
 		map = new int[H][W];
-		
+
 		for (int i = 0; i < H; i++) {
 			for (int j = 0; j < W; j++) {
 				map[i][j] = origin[i][j];
@@ -78,17 +81,29 @@ public class Solution {
 
 	// 3. 벽 연쇄 파괴하기
 	private static void destroy(int x, int y) {
-		int bound = map[x][y] - 1; // 구슬 맞은 벽돌의 폭발 범위
+		Queue<int[]> queue = new LinkedList<>();
+		queue.add(new int[] { x, y, map[x][y] });
 		map[x][y] = 0;
 		count++; // 파괴한 벽 개수 세기
 
-		for (int i = 0; i < 4; i++) {
-			for (int j = 1; j <= bound; j++) {
-				int nx = x + dir[i][0] * j;
-				int ny = y + dir[i][1] * j;
+		while (!queue.isEmpty()) {
+			int[] brick = queue.poll();
+			x = brick[0];
+			y = brick[1];
+			int bound = brick[2];
+			
+			for (int i = 0; i < 4; i++) {
+				for (int j = 1; j < bound; j++) {
+					int nx = x + dir[i][0] * j;
+					int ny = y + dir[i][1] * j;
 
-				if (nx < 0 || ny < 0 || nx >= H || ny >= W) continue;
-				if (map[nx][ny] != 0) destroy(nx, ny);
+					if (nx < 0 || ny < 0 || nx >= H || ny >= W) continue;
+					if (map[nx][ny] != 0){
+						queue.add(new int[] {nx, ny, map[nx][ny]});
+						map[nx][ny] = 0;
+						count++;
+					}
+				}
 			}
 		}
 
@@ -96,19 +111,21 @@ public class Solution {
 
 	// 4. 공백 채우기
 	private static void fillEmptySpace() {
-	    for (int y = 0; y < W; y++) {
-	        int end = H - 1;
-	        
-	        for (int x = H - 1; x >= 0; x--) {
-	            if (map[x][y] != 0) {
-	                map[end][y] = map[x][y];
-	                if (end != x) {
-	                    map[x][y] = 0; // 원래 위치를 0으로 설정하여 빈 칸 처리
-	                }
-	                end--;
-	            }
-	        }
-	    }
+
+		for (int y = 0; y < W; y++) {
+			Stack<Integer> stack = new Stack<>();
+			for (int x = 0; x < H; x++) {
+				if (map[x][y] != 0) {
+					stack.add(map[x][y]);
+					map[x][y] = 0;
+				}
+			}
+
+			int row = H - 1;
+			while (!stack.isEmpty()) {
+				map[row--][y] = stack.pop();
+			}
+		}
 	}
 
 }
