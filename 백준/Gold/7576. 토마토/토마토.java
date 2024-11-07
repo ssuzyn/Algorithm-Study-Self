02d8069a9bb72420aec2dc90fd89ae1ac77714dc
ssuzyn <88701965+ssuzyn@users.noreply.github.com>
2024-11-07 16:24:30 +0900

    [Gold V] Title: 토마토, Time: 564 ms, Memory: 120284 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -6,65 +6,61 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int N, M;
-	static int[][] tomato;
-	static int[] dx = {-1, 1, 0, 0};
-	static int[] dy = {0, 0, -1, 1};
-	static Queue<int[]> Q = new LinkedList<>();
 
-	public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        N = Integer.parseInt(st.nextToken()); // 열
-        M = Integer.parseInt(st.nextToken()); // 행
-        tomato = new int[M][N];
-        
-        for(int i = 0; i < M; i++) {
-        	st = new StringTokenizer(br.readLine());
-        	for(int j = 0; j < N; j++) {
-        		tomato[i][j] = Integer.parseInt(st.nextToken());
-        		if(tomato[i][j] == 1) Q.add(new int[] {i, j}); // 익은 토마토만 큐에 넣기
-        	}
-        }
-        
-        bfs();
-        
-        int day = 0;
-        for(int i = 0; i < M; i++) {
-        	for(int j = 0; j < N; j++) {
-        		if(tomato[i][j] == 0) {
-        			System.out.println(-1);
-        			return;
-        		}
-        		if(tomato[i][j] > day) {
-        			day = tomato[i][j];
-        		}
-        	}
-        }
-        
-        System.out.println(day-1);
-	}
-	
-	
-	static void bfs() {
+	static int N, M, count = 0, time = 0;
+	static int[][] map;
+	static Queue<int[]> tomato = new LinkedList<>();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken()); // 세로
+		M = Integer.parseInt(st.nextToken()); // 가로
 		
-		while(!Q.isEmpty()) {
-			int[] tmp = Q.poll();
-			
-			int x = tmp[0];
-			int y = tmp[1];
-			for(int i = 0; i < 4; i++) {
-				int nx = x + dx[i];
-				int ny = y + dy[i];
+		map = new int[M][N];
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
 				
-				if(nx >= 0 && nx < M && ny >= 0 && ny < N && tomato[nx][ny] == 0) {
-					tomato[nx][ny] = tomato[x][y] + 1;
-					Q.add(new int[] {nx, ny});
-				}
+				if (map[i][j] == 1) tomato.add(new int[] { i, j });
+				else if (map[i][j] == 0) count++;
 			}
 		}
+
+		makeTomato();
+		System.out.println(count == 0 ? time-1 : -1);
+	}
+	
+
+	private static void makeTomato() {
+		int[][] dir = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+
+		while (!tomato.isEmpty()) {
+			
+			int size = tomato.size();
+
+			for (int i = 0; i < size; i++) {
+
+				int[] cur = tomato.poll();
+				int x = cur[0];
+				int y = cur[1];
+
+				for (int[] d : dir) {
+					int nx = x + d[0];
+					int ny = y + d[1];
+
+					if (nx >= 0 && ny >= 0 && nx < M && ny < N && map[nx][ny] == 0){
+						tomato.add(new int[] { nx, ny });
+						map[nx][ny] = 1;
+						count--;
+					}
+				}
+			}
+			time++;
+		}
+
 	}
 
 }
