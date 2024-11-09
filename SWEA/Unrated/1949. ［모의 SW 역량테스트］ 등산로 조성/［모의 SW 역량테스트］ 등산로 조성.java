2788e4be6545495a0ce1f4ac4c71bce229cfc416
ssuzyn <88701965+ssuzyn@users.noreply.github.com>
2024-11-09 18:17:37 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 126 ms, Memory: 20,540 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
@@ -3,75 +3,80 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution {
-
-    static int N, K, ans, high;
-    static int[][] map;
-    static boolean[][] visited;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        int T = Integer.parseInt(br.readLine());
-        for (int t = 1; t <= T; t++) {
-            st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken()); // 지도의 한 변 길이
-            K = Integer.parseInt(st.nextToken()); // 최대 공사 가능 깊이
-            map = new int[N][N];
-            visited = new boolean[N][N];
-            high = 0;
-            ans = 0;
-
-            for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < N; j++) {
-                    map[i][j] = Integer.parseInt(st.nextToken());
-                    high = Math.max(high, map[i][j]);
-                }
-            }
-
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    if (map[i][j] == high) {
-                        visited[i][j] = true;
-                        dfs(i, j, 1, false); // cut을 boolean으로 처리 (false: 아직 안깎음)
-                        visited[i][j] = false;
-                    }
-                }
-            }
-
-            System.out.println("#" + t + " " + ans);
-        }
-    }
-
-    static void dfs(int x, int y, int depth, boolean cut) {
-
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (nx < 0 || nx >= N || ny < 0 || ny >= N || visited[nx][ny]) continue;
-
-            if (map[x][y] > map[nx][ny]) { // 현재 봉우리 > 다음 탐색할 봉우리
-                visited[nx][ny] = true;
-                dfs(nx, ny, depth + 1, cut);
-                visited[nx][ny] = false;
-            }
-            
-            if (map[x][y] <= map[nx][ny] && !cut) { // 아직 깎지 않은 경우에만
-                for (int j = 1; j <= K; j++) {
-                    if (map[nx][ny] - j < map[x][y]) { // 공사를 해서 갈 수 있는 경우
-                        map[nx][ny] -= j;
-                        dfs(nx, ny, depth + 1, true); // 깎은 후에는 cut=true
-                        map[nx][ny] += j;
-                    }
-                }
-            }
-        }
-
-        ans = Math.max(ans, depth); // 최댓값 갱신
-    }
+public class Solution{
+	
+	static int N, K, answer;
+	static int[][] map;
+	static boolean[][] visited;
+	static int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // 상하좌우 
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		for(int t = 1; t <= T; t++) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken()); // 지도 크기 
+			K = Integer.parseInt(st.nextToken()); // 최대 공사 가능 깊이 
+			answer = Integer.MIN_VALUE;
+			
+			map = new int[N][N];
+			visited = new boolean[N][N];
+			int high = 0;
+			for(int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j = 0; j < N; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+					high = Math.max(map[i][j], high);
+				}
+			}
+			
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < N; j++) {
+					if(map[i][j] == high) {
+						visited[i][j] = true;
+						dfs(i, j, 1, false);
+						visited[i][j] = false;
+					}
+				}
+			}
+			
+			
+			System.out.println("#" + t + " " + answer);
+			
+		}
+	}
+	
+	private static void dfs(int x, int y, int depth, boolean flag) {
+		answer = Math.max(depth, answer);
+		
+		for(int d = 0; d < 4; d++) {
+			int nx = x + dir[d][0];
+			int ny = y + dir[d][1];
+			
+			if(nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny]) continue;
+			
+			int tmpHeight = map[x][y]; // 현재 봉우리 
+			int nextHeight = map[nx][ny]; // 다음 봉우리 
+			
+			if(nextHeight < tmpHeight) { // 다음 봉우리 < 현재 봉우리 
+				visited[nx][ny] = true;
+				dfs(nx, ny, depth + 1, flag);
+				visited[nx][ny] = false;
+			}
+			else if(nextHeight >= tmpHeight && !flag) { // 다음 봉우리 >= 현재 봉우리 
+				for(int i = 1; i <= K; i++) {
+					if(nextHeight - i < tmpHeight) {
+						map[nx][ny] -= i;  
+						visited[nx][ny] = true;
+						dfs(nx, ny, depth + 1, true);
+						map[nx][ny] += i;
+						visited[nx][ny] = false;
+					}
+				}
+			}
+			
+		}
+	}
 }
