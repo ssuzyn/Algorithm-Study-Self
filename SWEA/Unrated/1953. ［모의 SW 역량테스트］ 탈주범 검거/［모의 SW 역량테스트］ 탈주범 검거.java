961a18560dad0d87a4ff044fdcd142d46255ceb6
ssuzyn <88701965+ssuzyn@users.noreply.github.com>
2024-11-09 16:47:46 +0900

    [Unrated] Title: [모의 SW 역량테스트] 탈주범 검거, Time: 132 ms, Memory: 23,856 KB -BaekjoonHub

--- SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
+++ SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
@@ -5,97 +5,69 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Solution {
-
-    static int count;
-    static int N, M, R, C, L;
-    static int[][] map;
-
-    // 방향: 상(0), 하(1), 좌(2), 우(3)
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-
-    // 파이프 방향 정의 (상하좌우)
-    static int[][] pipe = {
-        {0, 1, 2, 3}, // 1번 파이프: 상하좌우
-        {0, 1},       // 2번 파이프: 상하
-        {2, 3},       // 3번 파이프: 좌우
-        {0, 3},       // 4번 파이프: 상우
-        {1, 3},       // 5번 파이프: 하우
-        {1, 2},       // 6번 파이프: 하좌
-        {0, 2}        // 7번 파이프: 상좌
-    };
-
-    // 방향별 연결 가능한 파이프 번호
-    static int[][] connect = {
-        {1, 2, 5, 6}, // 상
-        {1, 2, 4, 7}, // 하
-        {1, 3, 4, 5}, // 좌
-        {1, 3, 6, 7}  // 우
-    };
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-
-        for(int t = 1; t <= T; t++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken()); // 터널의 세로 크기
-            M = Integer.parseInt(st.nextToken()); // 터널의 가로 크기
-            R = Integer.parseInt(st.nextToken()); // 맨홀 뚜껑이 위치한 행 좌표
-            C = Integer.parseInt(st.nextToken()); // 맨홀 뚜껑이 위치한 열 좌표
-            L = Integer.parseInt(st.nextToken()); // 탈출 후 소요된 시간
-            map = new int[N][M];
-            count = 1;
-
-            for(int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine());
-                for(int j = 0; j < M; j++) {
-                    map[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
-
-            bfs(R, C);
-            System.out.println("#" + t + " " + count);
-        }
-    }
-
-    static void bfs(int x, int y) {
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {x, y, map[x][y], 1});
-        map[x][y] = -1; // 방문 처리
-
-        while(!q.isEmpty()) {
-            int[] cur = q.poll();
-            x = cur[0];
-            y = cur[1];
-            int pipeType = cur[2];
-            int time = cur[3]; // 경과된 시간
-
-            if(time == L) continue; // 탈출 소요 시간이 된 경우 탐색 종료
-
-            for(int d : pipe[pipeType - 1]) {
-                int nx = x + dx[d];
-                int ny = y + dy[d];
-                int nextPipeType;
-
-                if(nx < 0 || nx >= N || ny < 0 || ny >= M || (nextPipeType = map[nx][ny]) <= 0) continue;
-
-                if(isConnected(d, nextPipeType - 1)) {
-                    q.offer(new int[] {nx, ny, nextPipeType, time + 1});
-                    map[nx][ny] = -1;
-                    count++;
-                }
-            }
-        }
-    }
-
-    static boolean isConnected(int dir, int nextPipeType) {
-        for(int connectPipe : connect[dir]) {
-            if(connectPipe == nextPipeType + 1) {
-                return true;
-            }
-        }
-        return false;
-    }
+public class Solution{
+	
+	static int N, M, startX, startY, time;
+	static int[][] map;
+	static int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // 상하좌우 
+	static int[][] connect = {{1, 2, 5, 6}, {1, 2, 4, 7}, {1, 3, 4, 5}, {1, 3, 6, 7}}; // 각 방향별로 연결 가능한 구조
+	static int[][] struct = {{0, 1, 2, 3}, {0, 1}, {2, 3}, {0, 3}, {1, 3}, {2, 1}, {2, 0}};
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		for(int t = 1; t <= T; t++) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken()); // 행 
+			M = Integer.parseInt(st.nextToken()); // 열 
+			startX = Integer.parseInt(st.nextToken()); // 출발 좌표 x
+			startY = Integer.parseInt(st.nextToken()); // 출발 좌표 y
+			time = Integer.parseInt(st.nextToken()); // 탈출 후 소요된 시간 
+			
+			map = new int[N][M];
+			for(int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j = 0; j < M; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			int cnt = 1; // 시작 지점 포함 
+			boolean[][] visited = new boolean[N][M];
+			Queue<int[]> q = new LinkedList<>();
+			q.add(new int[] {startX, startY, 1});
+			visited[startX][startY] = true;
+			
+			while(!q.isEmpty()) {
+				int[] cur = q.poll();
+				int x = cur[0];
+				int y = cur[1];
+				int depth = cur[2];
+				
+				if(depth == time) continue;
+				
+				int idx = map[x][y];
+				for(int d : struct[idx-1]) {
+					int nx = x + dir[d][0];
+					int ny = y + dir[d][1];
+					
+					if(nx < 0 || ny < 0 || nx >= N || ny >= M || visited[nx][ny]) continue;
+					if(map[nx][ny] > 0) { // 터널이 있는 위치라면 탐색 진행 
+						for(int i = 0; i < connect[d].length; i++) {
+							if(map[nx][ny] == connect[d][i]) {
+								q.add(new int[] {nx, ny, depth + 1});
+								visited[nx][ny] = true;
+								cnt++;
+							}
+						}
+					}
+				}
+			}
+			
+			System.out.println("#" + t + " " + cnt);
+			
+		}
+	}
 }
