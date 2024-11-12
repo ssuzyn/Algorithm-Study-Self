8705af657a3a1a4ea227a082f06cf0d443f57240
ssuzyn <88701965+ssuzyn@users.noreply.github.com>
2024-11-12 14:58:12 +0900

    [Gold III] Title: 아기 상어, Time: 156 ms, Memory: 21168 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -7,91 +7,105 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+	
+	static int N, full, time;
+	static int[][] map;
+	static Fish baby;
+	
+	static class Fish implements Comparable<Fish>{
+		int x, y, size, time;
+		
+		Fish(int x, int y, int size, int time){
+			this.x = x;
+			this.y = y;
+			this.size = size;
+			this.time = time;
+		}
 
-    static int N;
-    static int[][] d = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // 상하좌우
-    static int[][] map;
-    static PriorityQueue<Fish> pq;
+		@Override
+		public int compareTo(Fish o) {
+			if(this.time != o.time) return this.time - o.time;
+			else {
+				if(this.x != o.x) {
+					return this.x - o.x; // 가장 위에 있는 물고기 
+				}
+				return this.y - o.y;  // x좌표가 같다면 가장 왼쪽에 있는 물고기 
+			}
+		}
+		
+	}
 
-    static class Fish implements Comparable<Fish> {
-        int x, y, dist;
-
-        Fish(int x, int y, int dist) {
-            this.x = x;
-            this.y = y;
-            this.dist = dist;
-        }
-
-        @Override
-        public int compareTo(Fish o) {
-            if(this.dist != o.dist) return this.dist - o.dist;
-            else{
-                if(this.x == o.x) return this.y - o.y;
-                else return this.x - o.x;
-            }
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine()); // 공간의 크기
-        map = new int[N][N];
-
-        Queue<Fish> q = new LinkedList<>();
-        for(int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-                if(map[i][j] == 9) {
-                    map[i][j] = 0;
-                    q.add(new Fish(i, j, 0));
-                }
-            }
-        }
-
-        int sharkSize = 2;
-        int full = 0;
-        int time = 0;
-        bfs(q, sharkSize);
-
-        while(!pq.isEmpty()) {
-            Fish fish = pq.poll();
-            map[fish.x][fish.y] = 0;
-
-            if(++full == sharkSize){
-                full = 0;
-                sharkSize++;
-            }
-
-            time += fish.dist;
-            q = new LinkedList<>();
-            q.offer(new Fish(fish.x, fish.y, 0));
-            bfs(q, sharkSize);
-        }
-
-        System.out.println(time);
-    }
-
-    private static void bfs(Queue<Fish> q, int sharkSize) {
-        pq = new PriorityQueue<>();
-        boolean[][] visited = new boolean[N][N];
-
-        while(!q.isEmpty()) {
-            Fish fish = q.poll();
-
-            for(int[] dir : d){
-                int nx = fish.x + dir[0];
-                int ny = fish.y + dir[1];
-
-                if(nx < 0 || ny < 0 || nx >= N || ny >= N
-                                || map[nx][ny] > sharkSize || visited[nx][ny]) continue;
-                visited[nx][ny] = true;
-                q.offer(new Fish(nx, ny, fish.dist + 1));
-
-                if(map[nx][ny] != 0 && map[nx][ny] < sharkSize){
-                    pq.offer(new Fish(nx, ny, fish.dist + 1));
-                }
-            }
-        }
-    }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine()); // 맵 크기 
+		map = new int[N][N];
+		
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < N; j++) {
+				 map[i][j] = Integer.parseInt(st.nextToken());
+				 if(map[i][j] == 9) {
+					 map[i][j] = 0;
+					 baby = new Fish(i, j, 2, 0);
+				 }
+			}
+		}
+		
+		full = 0;
+		time = 0;
+		while(true) {
+			if(!eatBabyFish()) break;
+		}
+		
+		System.out.println(time);
+	}
+	
+	private static boolean eatBabyFish() {
+		boolean[][] visited = new boolean[N][N];
+		int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+		
+		Queue<int[]> fishQ = new LinkedList<>();
+		PriorityQueue<Fish> canEat = new PriorityQueue<>();
+		
+		fishQ.add(new int[] {baby.x, baby.y, 0});
+		visited[baby.x][baby.y] = true;
+		
+		while(!fishQ.isEmpty()) {
+			int[] cur = fishQ.poll();
+			int x = cur[0];
+			int y = cur[1];
+			int depth = cur[2];
+			
+			if(0 < map[x][y] && map[x][y] < baby.size) {
+				canEat.add(new Fish(x, y, map[x][y], depth));
+				continue;
+			}
+			
+			for(int i = 0; i < 4; i++) {
+				int nx = x + dir[i][0];
+				int ny = y + dir[i][1];
+				
+				if(nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny]) continue;
+				if(map[nx][ny] > baby.size) continue;
+				visited[nx][ny] = true;
+				fishQ.add(new int[] {nx, ny, depth+1});
+			}
+		}
+		
+		if(canEat.isEmpty()) return false;
+		
+		Fish fish = canEat.poll();
+		full++;
+		baby.x = fish.x;
+		baby.y = fish.y;
+		time += fish.time;
+		map[fish.x][fish.y] = 0;
+		
+		if(full == baby.size) {
+			baby.size++;
+			full = 0;
+		}
+		return true;
+		
+	}
 }
