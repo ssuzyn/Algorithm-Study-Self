4e8733286caa79e8f22899c2441a300f08d9e875
안수진 <88701965+ssuzyn@users.noreply.github.com>
2025-03-30 12:43:59 +0900

    [Gold IV] Title: 전화번호 목록, Time: 496 ms, Memory: 231056 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -1,9 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashMap;
 
 public class Main {
+	static class Node{
+		HashMap<Character, Node> child;
+		boolean endOfWord;
+
+		public Node(){
+			child = new HashMap<>();
+			endOfWord = false;
+		}
+	}
+
+	static class Trie{
+		Node root;
+
+		public Trie(){
+			root = new Node();
+		}
+
+		public boolean insert(String word){
+			Node current = root;
+
+			for(char tmp : word.toCharArray()){
+				current.child.putIfAbsent(tmp, new Node()); // 현재 문자에 해당하는 노드가 없으면 생성
+				current = current.child.get(tmp);
+
+				if(current.endOfWord) return false;
+			}
+
+			current.endOfWord = true; // 현재 단어의 끝을 표시
+
+			if(!current.child.isEmpty()) // 이 단어의 끝 노드에 자식이 있다면
+				return false;
+
+			return true;
+		}
+	}
+
+	private static boolean isConsistent(String[] nums){
+		Trie trie = new Trie();
+
+		for(String n : nums){
+			if(!trie.insert(n)) return false;
+		}
+		return true;
+	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,17 +60,8 @@ public class Main {
 			for(int i = 0; i < N; i++){
 				nums[i] = br.readLine();
 			}
-			Arrays.sort(nums);
 
-			boolean flag = true;
-			for(int i = 0; i < N-1; i++){
-				if(nums[i+1].startsWith(nums[i])){
-					sb.append("NO\n");
-					flag = false;
-					break;
-				}
-			}
-			if(flag) sb.append("YES\n");
+			sb.append(isConsistent(nums)? "YES\n" : "NO\n");
 		}
 		System.out.println(sb);
 	}
