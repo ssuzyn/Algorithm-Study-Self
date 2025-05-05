45a5cde91b5ab5a068e807299da1beac6b5fe27e
안수진 <88701965+ssuzyn@users.noreply.github.com>
2025-05-05 22:38:27 +0900

    [Silver III] Title: 소변기, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/3186. 소변기/소변기.java
+++ 백준/Silver/3186. 소변기/소변기.java
@@ -1,72 +1,62 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int useTime = sc.nextInt();
-		int endTime = sc.nextInt();
-		int size = sc.nextInt();
-		String input = sc.next();
-		
-		int recordSize = endTime + size + 1;
-		int[] record = new int[recordSize];
-		Arrays.fill(record, 0);
-		for(int i = 0; i < size; i++) {
-			record[i] = Character.getNumericValue(input.charAt(i));
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int useTime = Integer.parseInt(st.nextToken());
+		int endTime = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int size = N + endTime + 1;
+		int data[] = new int[size];
+
+
+		String input = br.readLine();
+		for(int i = 0; i < N; i++){
+			data[i] = input.charAt(i) - '0';
 		}
-		
-		
-		int one = 0;
+
 		int zero = 0;
-		int zeroIndex = 0;
-		boolean using = false;
-		
+		int one = 0;
+		int index = 0;
+		boolean used = false;
 		StringBuilder sb = new StringBuilder();
-		
-		for(int i = 0; i < recordSize; i++) {
-			
-			if(!using) {
-				if(record[i] == 1) {
-					one ++;
-					
-					if(one == useTime) {
-						using = true;
-						one = 0;
-					}
-				}
-				else {
-					one = 0;
-				}
-				
-			}
-			
-			if(using) {
-				if(record[i] == 0) {
-					zero ++;
-					zeroIndex = i;
-					
-					if(zero == endTime) {
-						sb.append(i + 1).append("\n");
+
+		for(int i = 0; i < size; i++){
+			if(used){ // 사용중인 경우
+				if(data[i] == 0){
+					zero++;
+					index = i;
+
+					if(zero == endTime){
+						sb.append(index + 1).append("\n");
 						zero = 0;
 						one = 0;
-						using = false;
+						used = false;
 					}
 				}
-				else {
-					zero = 0;
-				}
+				else zero = 0;
 			}
-			
+			else{ // 사용하지 않는 경우
+				if(data[i] == 1){
+					one++;
+
+					if(one == useTime){
+						used = true;
+						one = 0;
+					}
+				}
+				else one = 0;
+			}
 		}
-		
-		
-		if(sb.length() == 0)
-			sb.append("NIKAD").append("\n");
-		
-		System.out.println(sb.toString());
 
+		if(sb.length() == 0) sb.append("NIKAD");
+		System.out.println(sb);
 	}
-
 }
