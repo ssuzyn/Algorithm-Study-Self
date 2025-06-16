a2e1c0bb9e929443d3893e8cc400fcc1da2ac616
안수진 <88701965+ssuzyn@users.noreply.github.com>
2025-06-16 18:47:48 +0900

    [Gold IV] Title: N-Queen, Time: 5388 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -4,38 +4,41 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    static int N, answer;
-    static int[] queen;
+	static int[] queen;
+	static int N, answer;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        queen = new int[N+1];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
 
-        setQueens(1);
-        System.out.println(answer);
-    }
+		queen = new int[N + 1];
+		answer = 0;
+		setQueen(1);
+		System.out.println(answer);
+	}
 
-    private static void setQueens(int idx){
+	private static void setQueen(int idx){
+		if(!isAvailable(idx - 1)) return;
 
-        if(idx > N){
-            answer++;
-            return;
-        }
+		if(idx > N){
+			answer++;
+			return;
+		}
 
-        for(int i = 1; i <= N; i++){
-            queen[idx] = i;
-            if(isAvailable(idx)) setQueens(idx + 1);
-        }
-    }
+		for(int i = 1; i <= N; i++){
+			queen[idx] = i;
+			setQueen(idx + 1);
+		}
+	}
 
-    private static boolean isAvailable(int x){
-        for(int i = 1; i < x; i++){
-            // 같은 열에 있는 경우
-            if(queen[x] == queen[i]) return false;
-            // 같은 대각선에 있는 경우
-            if(x - i == Math.abs(queen[x] - queen[i])) return false;
-        }
-        return true;
-    }
+	private static boolean isAvailable(int rowNum){
+		for(int i = 1; i < rowNum; i++){
+
+			// 같은 열에 있거나 대각선에 있으면 안된다.
+			if(queen[i] == queen[rowNum]
+				|| rowNum - i == Math.abs(queen[i] - queen[rowNum]))
+				return false;
+		}
+		return true;
+	}
 }
