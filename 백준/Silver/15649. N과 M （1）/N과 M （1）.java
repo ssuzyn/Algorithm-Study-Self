9fd21d92440fd2848766ce5919125ea149b43f9a
안수진 <88701965+ssuzyn@users.noreply.github.com>
2025-07-01 17:17:08 +0900

    [Silver III] Title: N과 M (1), Time: 2088 ms, Memory: 78092 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,41 +1,43 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main {
-	
-	static int N;
-	static int M;
- 	static int []numbers;
-	static boolean[] isSelected;
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M = sc.nextInt();
-		
-		numbers = new int[M]; // 순열 저장 배열
-		isSelected = new boolean[N+1]; // 인덱스에 해당하는 숫자가 사용 중인지 저장하는 배열
+public class Main{
+
+	static int N, M;
+	static int[] numbers;
+	static boolean[] visited;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		numbers = new int[M];
+		visited = new boolean[N + 1];
+
 		permutation(0);
+
 	}
-	
-	
-	public static void permutation(int cnt) {
-		if(cnt == M) {
-			for(int num : numbers) {
+
+	private static void permutation(int cnt){
+		if(cnt == M){
+			for(int num : numbers){
 				System.out.print(num + " ");
 			}
 			System.out.println();
 			return;
 		}
-		
-		for(int i = 1; i <= N; i++) {
-			if(isSelected[i]) continue;
-			
+
+		for(int i = 1; i <= N; i++){
+			if(visited[i]) continue;
 			numbers[cnt] = i;
-			isSelected[i] = true;
-			
+			visited[i] = true;
 			permutation(cnt + 1);
-			
-			isSelected[i] = false; // 다음 재귀호출에 영향을 주지 않기 위해 백트래킹
+			visited[i] = false;
 		}
 	}
 }
