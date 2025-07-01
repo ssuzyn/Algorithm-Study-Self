1773dbcb6c9cd0076dd66c53ef7d61a81c757acf
안수진 <88701965+ssuzyn@users.noreply.github.com>
2025-07-01 17:21:48 +0900

    [Silver III] Title: N과 M (2), Time: 120 ms, Memory: 15840 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,37 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main {
-	
+public class Main{
+
 	static int N, M;
 	static int[] numbers;
-	
-	public static void main(String[] args) throws IOException{
+	static boolean[] visited;
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] input = br.readLine().split(" ");
-		
-		N = Integer.parseInt(input[0]);
-		M = Integer.parseInt(input[1]);
-		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
 		numbers = new int[M];
+		visited = new boolean[N + 1];
+
 		permutation(0, 1);
-		
+
 	}
-	
-	private static void permutation(int cnt, int start) {
-		if(cnt == M) {
-			for(int num : numbers) {
+
+	private static void permutation(int cnt, int start){
+		if(cnt == M){
+			for(int num : numbers){
 				System.out.print(num + " ");
 			}
 			System.out.println();
 			return;
 		}
-		
-		for(int i = start; i <= N; i++) {
+
+		for(int i = start; i <= N; i++){
 			numbers[cnt] = i;
-			permutation(cnt+1, i+1);
+			permutation(cnt + 1, i + 1);
 		}
 	}
-	
 }
