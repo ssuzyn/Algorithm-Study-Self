775104f879348d054306ad7dff4138b2f1d4facd
안수진 <88701965+ssuzyn@users.noreply.github.com>
2025-07-14 15:15:50 +0900

    [Gold II] Title: 보석 도둑, Time: 1204 ms, Memory: 124824 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -1,15 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	static int N, K;
-	static int[] bag;
 
 	static class Jewel{
 		int weight;
@@ -24,38 +20,33 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken()); // 보석 개수
-		K = Integer.parseInt(st.nextToken()); // 가방 개수
-		bag = new int[K];
-		Jewel[] jewelList = new Jewel[N];
+		int N = Integer.parseInt(st.nextToken()); // 보석 개수
+		int K = Integer.parseInt(st.nextToken()); // 가방 개수
+
+		PriorityQueue<Integer> bag = new PriorityQueue<>();
+		PriorityQueue<Jewel> jewels = new PriorityQueue<>((j1, j2) -> j1.weight - j2.weight);
+		PriorityQueue<Integer> price = new PriorityQueue<>(Collections.reverseOrder());
 
 		for(int i = 0; i < N; i++){
 			st = new StringTokenizer(br.readLine());
-			jewelList[i] = new Jewel(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+			jewels.offer(new Jewel(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
 		}
 
 		for(int i = 0; i < K; i++){
-			bag[i] = Integer.parseInt(br.readLine());
+			bag.offer(Integer.parseInt(br.readLine()));
 		}
-
-		// 보석을 무게 순으로 정렬
-		Arrays.sort(jewelList, (j1, j2) -> j1.weight - j2.weight);
-
-		// 가방을 무게 순으로 정렬
-		Arrays.sort(bag);
-
-		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
 
 		long answer = 0;
-		int jewelIdx = 0;
 
-		for(int i = 0; i < K; i++){
-			while(jewelIdx < N && jewelList[jewelIdx].weight <= bag[i]){
-				pq.add(jewelList[jewelIdx].price);
-				jewelIdx++;
+		while(!bag.isEmpty()){
+			int curBag = bag.poll();
+
+			while(!jewels.isEmpty()){
+				if(jewels.peek().weight > curBag) break;
+				price.add(jewels.poll().price);
 			}
 
-			if(!pq.isEmpty()) answer += pq.poll();
+			if(!price.isEmpty()) answer += price.poll();
 		}
 
 		System.out.println(answer);
