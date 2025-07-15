ce236bb5eabbdb8dca5e5b8421cd6ba72117c678
안수진 <88701965+ssuzyn@users.noreply.github.com>
2025-07-15 11:59:52 +0900

    [Silver I] Title: 오목, Time: 72 ms, Memory: 11760 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.java
+++ 백준/Silver/2615. 오목/오목.java
@@ -7,74 +7,73 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[] dx = {1, 0, -1, 1}; // 하, 우, 우하 대각선, 좌하 대각선
-    static int[] dy = {0, 1, 1, 1};
-    static int[][] baduk = new int[19][19];
-    static List<int[]> position = new ArrayList<>();
+	static int[][] map = new int[19][19];
+	static int[][] dir = new int[][]{{0, 1}, {1, 0}, {1, 1}, {-1, 1}};
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        for(int i = 0; i < 19; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < 19; j++){
-                baduk[i][j] = Integer.parseInt(st.nextToken());
-                if(baduk[i][j] > 0) position.add(new int[]{i, j});
-            }
-        }
+		for(int i = 0; i < 19; i++){
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < 19; j++){
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
 
-        for(int[] p : position){
-            int x = p[0];
-            int y = p[1];
-            if(dfs(x, y)){
-                System.out.println(baduk[x][y]);
-                System.out.println((x+1) + " " + (y+1));
-                return;
-            }
-        }
-        System.out.println(0);
+		for(int i = 0; i < 19; i++){
+			for(int j = 0; j < 19; j++){
+				if(map[i][j] > 0 && dfs(i, j)){
+					System.out.println(map[i][j]);
+					System.out.println((i+1) + " " + (j+1));
+					return;
+				}
+			}
+		}
 
-    }
+		System.out.println(0);
+	}
 
-    static boolean dfs(int x, int y){
-        int color = baduk[x][y];
+	private static boolean dfs(int x, int y){
+		int color = map[x][y];
 
-        for(int d = 0; d < 4; d++){
-            int count = 1;
+		for(int d = 0; d < 4; d++){
+			// 1. 현재 위치가 이 방향의 시작점인지 확인
+			int prevX = x - dir[d][0];
+			int prevY = y - dir[d][1];
 
-            for(int i = 1; i < 5; i++) {
-                int nx = x + dx[d] * i;
-                int ny = y + dy[d] * i;
+			if(isValid(prevX, prevY) && map[prevX][prevY] == color){
+				continue; // 시작점이 아니므로 스킵
+			}
 
-                if (isNotBoundary(nx, ny) || isNotSameColor(nx, ny, color)) break;
-                count++;
-            }
+			// 2. 현재 방향으로 연속된 같은 색의 바둑돌 개수 세기
+			int count = 1;
 
-            // 5개의 돌이 연속된 경우에만 처리
-            if (count == 5) {
-                // 연속된 5개 돌의 앞과 뒤에 돌이 있는지 확인
-                int prevX = x - dx[d];
-                int prevY = y - dy[d];
-                int nextX = x + dx[d] * 5;
-                int nextY = y + dy[d] * 5;
+			for(int i = 1; i < 5; i++){
+				int nx = x + dir[d][0] * i;
+				int ny = y + dir[d][1] * i;
 
-                // 6목이 아닌지 확인 (앞뒤에 돌이 있으면 안됨)
-                if ((isNotBoundary(prevX, prevY) || isNotSameColor(prevX, prevY, color)) &&
-                    (isNotBoundary(nextX, nextY) || isNotSameColor(nextX, nextY, color)))  {
-                    return true; // 승리 조건 성립
-                }
-            }
-        }
+				if(isValid(nx, ny) && color == map[nx][ny]){
+					count++;
+				}
+				else{
+					break;
+				}
+			}
 
-        return false;
-    }
+			// 3. 정확히 5개인 경우 승리
+			if(count == 5){
+				int nextX = x + dir[d][0] * 5;
+				int nextY = y + dir[d][1] * 5;
 
-    static boolean isNotBoundary(int x, int y){
-        return x < 0 || y < 0 || x >= 19 || y >= 19;
-    }
+				if(!isValid(nextX, nextY) || color != map[nextX][nextY]) return true;
+			}
 
-    static boolean isNotSameColor(int x, int y, int color){
-        return baduk[x][y] != color;
-    }
+		}
+		return false;
+	}
+
+	private static boolean isValid(int x, int y){
+		return x >= 0 && y >= 0 && x < 19 && y < 19;
+	}
 
 }
