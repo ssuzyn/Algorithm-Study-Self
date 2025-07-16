8a049d4d7f8f8808f5c1f193692047bcb52afb0a
안수진 <88701965+ssuzyn@users.noreply.github.com>
2025-07-16 18:18:47 +0900

    [Gold V] Title: 암호 만들기, Time: 184 ms, Memory: 16244 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int L, C;
+	static String vowels = "aeiou";
+	static String[] alpha;
+	static String[] password;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		L = Integer.parseInt(st.nextToken()); // 암호 글자수
+		C = Integer.parseInt(st.nextToken()); // 문자의 종류
+		alpha = br.readLine().split(" ");
+		password = new String[L];
+
+		Arrays.sort(alpha);
+		dfs(0, 0, 0, 0);
+	}
+
+	static void dfs(int cnt, int idx, int vowel, int consonant){
+		if(cnt == L){
+			if(vowel >= 1 && consonant >= 2){
+				for(String word : password){
+					System.out.print(word);
+				}
+				System.out.println();
+			}
+			return;
+		}
+
+		if(idx == C) return;
+
+		password[cnt] = alpha[idx];
+
+		int nextVowel = vowel;
+		int nextConsonant = consonant;
+		if(vowels.contains(alpha[idx])) nextVowel++;
+		else nextConsonant++;
+
+		dfs(cnt + 1, idx + 1, nextVowel, nextConsonant);
+		dfs(cnt, idx + 1, vowel, consonant);
+	}
+
+}
