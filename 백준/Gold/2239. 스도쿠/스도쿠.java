e76777361c3fd00b2344d44135b7f2094421e9fd
안수진 <88701965+ssuzyn@users.noreply.github.com>
2025-07-17 22:43:18 +0900

    [Gold IV] Title: 스도쿠, Time: 404 ms, Memory: 17876 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.java
+++ 백준/Gold/2239. 스도쿠/스도쿠.java
@@ -1,79 +1,81 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
-    static int[][] map = new int[9][9];
-    static boolean solved = false; // 첫 해를 찾으면 중단
+	static int[][] sudoku;
+	static boolean[][][] check;
+	static List<int[]> zero;
+	static boolean flag;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sudoku = new int[9][9];
+		check = new boolean[3][9][10];
+		zero = new ArrayList<>();
+		flag = false;
 
-        for (int i = 0; i < 9; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < 9; j++) {
-                map[i][j] = line.charAt(j) - '0';
-            }
-        }
+		for(int i = 0; i < 9; i++){
+			String line = br.readLine();
+			for(int j = 0; j < 9; j++){
+				int num = line.charAt(j) - '0';
+				sudoku[i][j] = num;
+				if(num == 0) zero.add(new int[]{i, j});
+				else{
+					check[0][i][num] = true; // i번째 행에 num이 있다.
+					check[1][j][num] = true; // j번째 행에 num이 있다.
+					check[2][getSection(i, j)][num] = true; // 해당 박스구역에 num이 있다.
+				}
+			}
+		}
 
-        sudoku(0, 0);
+		solve(0);
 
-        // 결과 출력
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                System.out.print(map[i][j]);
-            }
-            System.out.println();
-        }
-    }
+		for(int i = 0; i < 9; i++){
+			for(int j = 0; j < 9; j++){
+				System.out.print(sudoku[i][j]);
+			}
+			System.out.println();
+		}
+	}
 
-    private static void sudoku(int x, int y) {
-        if (solved) return;
+	private static int getSection(int r, int c){
+		int row = r/3;
+		int col = c/3;
+		return row * 3 + col;
+	}
 
-        if (x == 9) {
-            solved = true;
-            return;
-        }
+	private static boolean isValid(int r, int c, int num){
+		return !(check[0][r][num] || check[1][c][num] || check[2][getSection(r, c)][num]);
+	}
 
-        if (map[x][y] > 0) {
-            if (y == 8) {
-                sudoku(x + 1, 0);
-            } else {
-                sudoku(x, y + 1);
-            }
-        } else {
-            // 빈 칸일 경우, 숫자를 넣어본다
-            for (int i = 1; i <= 9; i++) {
-                if (check(x, y, i)) {
-                    map[x][y] = i;
-                    if (y == 8) {
-                        sudoku(x + 1, 0);
-                    } else {
-                        sudoku(x, y + 1);
-                    }
-                    if (solved) return; // 이미 해결된 경우 중단
-                    map[x][y] = 0;
-                }
-            }
-        }
-    }
+	static boolean solve(int index){
+		if(index == zero.size()) return true;
 
-    private static boolean check(int x, int y, int target) {
-        // 행과 열 확인
-        for (int i = 0; i < 9; i++) {
-            if (map[x][i] == target || map[i][y] == target) return false;
-        }
-        
-        // 3x3 박스 확인
-        int startX = (x / 3) * 3;
-        int startY = (y / 3) * 3;
-        for (int i = startX; i < startX + 3; i++) {
-            for (int j = startY; j < startY + 3; j++) {
-                if (map[i][j] == target) return false;
-            }
-        }
+		int[] cur = zero.get(index);
+		for(int num = 1; num <= 9; num++){
+			int r = cur[0];
+			int c = cur[1];
+			if(isValid(r, c, num)){
+				sudoku[r][c] = num;
+				check[0][r][num] = true;
+				check[1][c][num] = true;
+				check[2][getSection(r, c)][num] = true;
 
-        return true;
-    }
+				if(solve(index + 1)) return true;
+
+				// 백트래킹
+				sudoku[r][c] = 0;
+				check[0][r][num] = false;
+				check[1][c][num] = false;
+				check[2][getSection(r, c)][num] = false;
+
+			}
+		}
+
+		return false;
+	}
 }
