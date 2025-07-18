a8642ee80d48bb90c0cb18596397267cf635a2f5
안수진 <88701965+ssuzyn@users.noreply.github.com>
2025-07-18 11:30:15 +0900

    [Gold III] Title: 줄 세우기, Time: 428 ms, Memory: 46752 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -3,64 +3,57 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static ArrayList<Integer> answer;
-	static ArrayList<Integer>[] graph;
+
+	static int N, M;
+	static List<Integer>[] height;
 	static int[] inDegree;
 
-
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		
-		answer = new ArrayList<>();
-		graph = new ArrayList[N+1];
-		inDegree = new int[N + 1]; // 각 정점의 진입 차수 저장
-		
-		for(int i = 1; i <= N; i++) {
-			graph[i] = new ArrayList<>();
+		N = Integer.parseInt(st.nextToken()); // 학생 수
+		M = Integer.parseInt(st.nextToken()); // 키를 비교한 횟수
+		height = new ArrayList[N + 1];
+		inDegree = new int[N + 1];
+
+		for(int i = 1; i <= N; i++){
+			height[i] = new ArrayList<>();
 		}
-		
-		for(int i = 0; i < M; i++) {
+
+		for(int i = 0; i < M; i++){
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			graph[a].add(b);
-			inDegree[b]++;
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			height[A].add(B); // A < B
+			inDegree[B]++;
 		}
-		
+
 		topology();
-		
-		for(int s : answer) {
-			System.out.print(s + " ");
-		}
-	}
-	
-	private static void topology() {
-		Queue<Integer> q = new LinkedList<>();
-		
-		for(int i = 1; i < inDegree.length; i++) {
-			if(inDegree[i] == 0) q.add(i);
-		}
-		
-		while(!q.isEmpty()) {
-			int tmp = q.poll();
-			answer.add(tmp);
-			
-			for(int i = 0; i < graph[tmp].size(); i++) {
-				inDegree[graph[tmp].get(i)]--;
-				
-				if(inDegree[graph[tmp].get(i)] == 0)
-					q.add(graph[tmp].get(i));
-			}
-		}
 	}
 
+	private static void topology(){
+		Queue<Integer> q = new LinkedList<>();
+		StringBuilder sb = new StringBuilder();
+
+		for(int i = 1; i <= N; i++){
+			if(inDegree[i] == 0) q.add(i);
+		}
+
+		while(!q.isEmpty()){
+			int cur = q.poll();
+			sb.append(cur).append(" ");
+
+			for(int num : height[cur]){
+				inDegree[num]--;
+				if(inDegree[num] == 0) q.add(num);
+			}
+		}
+
+		System.out.println(sb);
+	}
 }
