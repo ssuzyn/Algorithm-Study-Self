3c0e6d78a49c09e5ff09733a9c68ab998d6e8ca7
안수진 <88701965+ssuzyn@users.noreply.github.com>
2025-07-21 15:43:14 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 132 ms, Memory: 14896 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
@@ -1,81 +1,66 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N, beer = 20;
-	static int[][] map;
-	static Node startNode, endNode;
-	static List<Node> store;
-	
-	static class Node{
-		int x, y;
-		
-		Node(int x, int y){
+	static int N;
+	static Point[] points;
+
+	static class Point{
+		int x; int y;
+
+		Point(int x, int y){
 			this.x = x;
 			this.y = y;
 		}
 	}
-	
-	public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        
-        StringTokenizer st;
-        for(int t = 1; t <= T; t++) {
-        	N = Integer.parseInt(br.readLine()); // 편의점 개수
-        	map = new int[N][N];
-        	
-        	st = new StringTokenizer(br.readLine());
-        	startNode = new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-        	
-        	store = new ArrayList<>();
-        	for(int i = 0; i < N; i++) {
-        		st = new StringTokenizer(br.readLine());
-        		store.add(new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
-        	}
-        	
-        	st = new StringTokenizer(br.readLine());
-        	endNode = new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-        	
-        	goFestival();
-        }
-        
-	}
-	
-	private static void goFestival() {
-		Queue<Node> q = new LinkedList<>();
-		q.add(startNode);
-		boolean[] visited = new boolean[store.size()];
-		
-		while(!q.isEmpty()) {
-			Node tmp = q.poll();
-			
-			if(Math.abs(tmp.x - endNode.x) + Math.abs(tmp.y - endNode.y) <= beer * 50) {
-				System.out.println("happy");
-				return;
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int tc = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		while(tc-- > 0){
+			N = Integer.parseInt(br.readLine());
+			points = new Point[N + 2];
+
+			for(int i = 0; i < N + 2; i++){
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				points[i] = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
 			}
-			
-			for(int i = 0; i < store.size(); i++) {
-				
+
+			if(bfs()) sb.append("happy\n");
+			else sb.append("sad\n");
+		}
+
+		System.out.print(sb);
+	}
+
+
+	private static boolean bfs(){
+		Queue<Point> q = new LinkedList<>();
+		boolean[] visited = new boolean[points.length];
+		visited[0] = true;
+		q.add(points[0]);
+
+		while(!q.isEmpty()){
+			Point cur = q.poll();
+			if(cur.x == points[N+1].x && cur.y == points[N+1].y) return true;
+
+			for(int i = 0; i < N + 2; i++){
 				if(visited[i]) continue;
-				
-				Node next = store.get(i);
-				
-				if(Math.abs(tmp.x - next.x) + Math.abs(tmp.y - next.y) <= beer * 50) {
+				int dist = Math.abs(cur.x - points[i].x) + Math.abs(cur.y - points[i].y);
+				if(dist <= 1000) {
 					visited[i] = true;
-					q.add(next);
+					q.add(points[i]);
 				}
 			}
 		}
-		
-		System.out.println("sad");
-		return;
+
+		return false;
 	}
 }
