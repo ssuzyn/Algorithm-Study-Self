2a4c610a9ec75cd787a44589c72451f4cdc7caa3
안수진 <88701965+ssuzyn@users.noreply.github.com>
2025-07-22 17:35:35 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 188 ms, Memory: 17392 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -7,89 +7,11 @@ public class Main {
 
 	static int N, count;
 	static int[][] map;
-	static boolean[][] visited;
-	
-	private static int getDirection(int diffX, int diffY) {
-		if(diffX == 0 && diffY == 1) return 0; //가로 방향
-		else if(diffX == 1 && diffY == 0) return 1; //세로 방향
-		else if(diffX == 1 && diffY == 1) return 2; //대각선 방향
-		
-		return -1;
-	}
-	
-	private static boolean isValid(int x, int y) {
-		return x >= 0 && y >= 0 && x < N && y < N;
-	}
-	
-	private static void moveHorizontal(int x1, int y1, int x2, int y2) {
-		x1 = x2;
-		y1 = y2;
-		x2 = x2 + 0;
-		y2 = y2 + 1;
-			
-		if(isValid(x2, y2) && !visited[x2][y2] && map[x2][y2] == 0) {
-			visited[x2][y2] = true;
-			dfs(x1, y1, x2, y2);
-			visited[x2][y2] = false;
-		}	
-		
-	}
-	
-	private static void moveVertical(int x1, int y1, int x2, int y2) {
-		x1 = x2;
-		y1 = y2;
-		x2 = x2 + 1;
-		y2 = y2 + 0;
-			
-		if(isValid(x2, y2) && !visited[x2][y2] && map[x2][y2] == 0) {
-			visited[x2][y2] = true;
-			dfs(x1, y1, x2, y2);
-			visited[x2][y2] = false;
-		}	
-		
-	}
-	
-	private static void moveDiagonal(int x1, int y1, int x2, int y2) {
-		x1 = x2;
-		y1 = y2;
-		x2 = x2 + 1;
-		y2 = y2 + 1;
-			
-		if(isValid(x2, y2) && !visited[x2][y2] && map[x2][y2] == 0 && map[x2-1][y2] == 0 && map[x2][y2-1] == 0) {
-			visited[x2][y2] = true;
-			dfs(x1, y1, x2, y2);
-			visited[x2][y2] = false;
-		}	
-		
-	}
-	
-	private static void dfs(int x1, int y1, int x2, int y2) {
-		if(x2 == N -1 && y2 == N -1) count++;
-		
-		int flag = getDirection(x2 - x1, y2 - y1);
-		
-		switch(flag) {
-		case 0:
-			moveHorizontal(x1, y1, x2, y2);
-			moveDiagonal(x1, y1, x2, y2);
-			break;
-		case 1:
-			moveVertical(x1, y1, x2, y2);
-			moveDiagonal(x1, y1, x2, y2);
-			break;
-		case 2:
-			moveHorizontal(x1, y1, x2, y2);
-			moveVertical(x1, y1, x2, y2);
-			moveDiagonal(x1, y1, x2, y2);
-			break;
-		}
-	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		map = new int[N][N];
-		visited = new boolean[N][N];
 		
 		StringTokenizer st;
 		for(int i = 0; i < N; i++) {
@@ -99,9 +21,35 @@ public class Main {
 			}
 		}
 		
-		dfs(0, 0, 0, 1);
+		dfs(0, 1, 1); // 1 : 가로, 2 : 세로, 3 : 대각선
 		
 		System.out.println(count);
 	}
+	
+	private static void dfs(int x, int y, int status) {
+		if(x == N - 1 && y == N - 1) {
+			count++;
+			return;
+		}
+		
+		// 수평으로 이동 가능
+		if(status == 1 || status == 3) { 
+			if(y + 1 < N && map[x][y+1] != 1) 
+				dfs(x, y+1, 1);
+		}
+		
+		// 수직으로 이동 가능
+		if(status == 2 || status == 3) { 
+			if(x + 1 < N && map[x+1][y] != 1)
+				dfs(x+1, y, 2);
+		}
+
+		// 대각선으로 이동 가능
+		if(x+1 < N && y+1 < N && map[x+1][y+1] != 1 && map[x][y+1] != 1 && map[x+1][y] != 1) {
+			dfs(x+1, y+1, 3);
+		}
+		
+	}
 
 }
+
