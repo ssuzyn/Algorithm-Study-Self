f52510a2a3db4d1150d6fd1f88c5f29463b4b3c8
안수진 <88701965+ssuzyn@users.noreply.github.com>
2025-07-22 18:48:12 +0900

    [Gold V] Title: 테트리스, Time: 108 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Gold/3019. 테트리스/테트리스.java
+++ 백준/Gold/3019. 테트리스/테트리스.java
@@ -1,85 +1,125 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
 
-	static int c, p, cnt;
-	static int[] map;
+	static int C, P;
+	static int[] arr;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		c = Integer.parseInt(st.nextToken());
-		p = Integer.parseInt(st.nextToken());
-		map = new int[c];
+		C = Integer.parseInt(st.nextToken()); // 열
+		P = Integer.parseInt(st.nextToken()); // 블록의 번호
+		arr = new int[C];
 
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < c; i++)
-			map[i] = Integer.parseInt(st.nextToken());
-
-		switch (p) {
-			case 1:
-				cnt += c;
-				check("0000");
-				break;
-			case 2:
-				check("00");
-				break;
-			case 3:
-				check("001");
-				check("10");
-				break;
-			case 4:
-				check("100");
-				check("01");
-				break;
-			case 5:
-				check("000");
-				check("01");
-				check("101");
-				check("10");
-				break;
-			case 6:
-				check("000");
-				check("00");
-				check("011");
-				check("20");
-				break;
-			case 7:
-				check("000");
-				check("02");
-				check("110");
-				check("00");
-				break;
+		for(int i = 0; i < C; i++){
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 
-		bw.write(cnt + "\n");
-		bw.flush();
+		System.out.println(solve());
 	}
 
-	private static void check(String block) {
-		int len = block.length();
+	private static int solve(){
+		int ans = 0;
 
-		for (int i = 0; i <= c - len; i++) {
-			int gap = map[i] - block.charAt(0) - '0';
+		if(P == 1){ // (1x4), (4x1) 도형
+			ans += C;
 
-			boolean flag = true;
-			for (int j = 1; j < len; j++) {
-				if (map[i + j] - block.charAt(j) - '0' != gap) {
-					flag = false;
-					break;
-				}
+			for(int i = 0; i <= C - 4; i++){
+				ans += isSameLevel(i, 4);
+			}
+		}
+		else if(P == 2){ // 정사각형
+			for(int i = 0; i <= C - 2; i++){
+				ans += isSameLevel(i, 2);
+			}
+		}
+		else if(P == 3){
+			for(int i = 0; i <= C - 3; i++){
+				if(arr[i] == arr[i+1] && arr[i+1] + 1 == arr[i+2]) ans++;
 			}
 
-			if (flag)
-				cnt++;
+			for(int i = 0; i <= C - 2; i++){
+				if(arr[i] == arr[i+1] + 1) ans++;
+			}
 		}
+		else if(P == 4){
+			for(int i = 0; i <= C - 3; i++){
+				if(arr[i] == arr[i+1] + 1 && arr[i+1] == arr[i+2]) ans++;
+			}
+
+			for(int i = 0; i <= C - 2; i++){
+				if(arr[i] + 1 == arr[i+1]) ans++;
+			}
+		}
+		else if(P == 5){
+			for(int i = 0; i <= C - 3; i++){ // ㅗ
+				ans += isSameLevel(i, 3);
+			}
+
+			for(int i = 0; i <= C - 2; i++){ // ㅏ
+				if(arr[i] + 1 == arr[i+1]) ans++;
+			}
+
+			for(int i = 0; i <= C - 3; i++){ // ㅜ
+				if(arr[i] == arr[i+1] + 1 && arr[i+1] + 1 == arr[i+2]) ans++;
+			}
+
+			for(int i = 0; i <= C - 2; i++){ // ㅓ
+				if(arr[i] == arr[i+1] + 1) ans++;
+			}
+		}
+		else if(P == 6){
+			for(int i = 0; i <= C - 3; i++){
+				ans += isSameLevel(i, 3);
+			}
+
+			for(int i = 0; i <= C - 2; i++){
+				ans += isSameLevel(i, 2);
+			}
+
+			for(int i = 0; i <= C - 3; i++){
+				if(arr[i] + 1 == arr[i+1] && arr[i+1] == arr[i+2]) ans++;
+			}
+
+			for(int i = 0; i <= C - 2; i++){
+				if(arr[i] == arr[i+1] + 2) ans++;
+			}
+		}
+		else if(P == 7){
+			for(int i = 0; i <= C - 3; i++){
+				ans += isSameLevel(i, 3);
+			}
+
+			for(int i = 0; i <= C - 2; i++){
+				if(arr[i] + 2 == arr[i+1]) ans++;
+			}
+
+			for(int i = 0; i <= C - 3; i++){
+				if(arr[i] == arr[i+1] && arr[i+1] == arr[i+2] + 1) ans++;
+			}
+
+			for(int i = 0; i <= C - 2; i++){
+				ans += isSameLevel(i, 2);
+			}
+
+		}
+
+		return ans;
+	}
+
+	private static int isSameLevel(int idx, int size){
+		int h = arr[idx];
+		for(int i = idx + 1; i < idx + size; i++){
+			if(arr[i] != h) return 0;
+		}
+
+		return 1;
 	}
 
 }
