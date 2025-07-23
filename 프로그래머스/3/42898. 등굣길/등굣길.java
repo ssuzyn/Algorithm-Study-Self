133217a45f00861e1b126c6487f1b92f95f61f9c
안수진 <88701965+ssuzyn@users.noreply.github.com>
2025-07-23 16:02:15 +0900

    [level 3] Title: 등굣길, Time: 0.67 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -9,10 +9,14 @@ class Solution {
         map[1][1] = 1;
         for(int i = 1; i <= m; i++){
             for(int j = 1; j <= n; j++){
-                if(map[i][j] == -1) continue;
+                if(map[i][j] == -1){
+                    map[i][j] = 0;
+                    continue;
+                }
                 
-                if(map[i-1][j] > 0) map[i][j] = (map[i][j] + map[i-1][j]) % 1000000007;
-                if(map[i][j-1] > 0) map[i][j] = (map[i][j] + map[i][j-1]) % 1000000007;
+                if(i == 1 && j == 1) continue;
+                
+                map[i][j] = (map[i-1][j] + map[i][j-1]) % 1000000007;
             }
         }
         
