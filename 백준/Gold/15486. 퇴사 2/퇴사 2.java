0189e2ced2e54c8cd9f34ebb2e5a160b98af0c5b
안수진 <88701965+ssuzyn@users.noreply.github.com>
2025-07-28 15:58:46 +0900

    [Gold V] Title: 퇴사 2, Time: 664 ms, Memory: 315868 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -5,28 +5,31 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] T = new int[N + 2]; // 상담을 완료하는데 걸리는 기간
-        int[] P = new int[N + 2]; // 상담 금액
-        int[] dp = new int[N + 2];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] T = new int[N + 2]; // 상담을 완료하는데 걸리는 기간
+		int[] P = new int[N + 2]; // 상담 금액
+		int[] dp = new int[N + 2];
 
-        StringTokenizer st;
-        for(int i = 1; i <= N; i++){
-            st = new StringTokenizer(br.readLine());
-            T[i] = Integer.parseInt(st.nextToken());
-            P[i] = Integer.parseInt(st.nextToken());
-        }
+		StringTokenizer st;
+		for(int i = 1; i <= N; i++){
+			st = new StringTokenizer(br.readLine());
+			T[i] = Integer.parseInt(st.nextToken());
+			P[i] = Integer.parseInt(st.nextToken());
+		}
 
-        for(int i = 1; i <= N; i++){
-            if(i + T[i] <= N + 1){
-                dp[i + T[i]] = Math.max(dp[i + T[i]], dp[i] + P[i]); // i일에 상담 하는 경우
-            }
-            dp[i + 1] = Math.max(dp[i + 1], dp[i]); // i일에 상담하지 않는 경우
-        }
+		int max = -1;
+		for(int day = 1; day <= N + 1; day++){
+			if(max < dp[day]) max = dp[day];
 
-        System.out.println(dp[N + 1]);
-    }
+			int endDate = day + T[day];
+			if(endDate <= N + 1){
+				dp[endDate] = Math.max(dp[endDate], max + P[day]);
+			}
+		}
+
+		System.out.println(dp[N + 1]);
+	}
 
 }
