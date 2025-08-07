4e86d7e8601e643a25685c299c0f57569945811b
안수진 <88701965+ssuzyn@users.noreply.github.com>
2025-08-07 15:27:51 +0900

    [Gold IV] Title: 연구소, Time: 336 ms, Memory: 146400 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,107 +1,91 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int N, M, maxSize;
-	static int[][] origin;
-	static int[][] map;
-	static boolean[][] visited;
-	static int[][] d = {{-1, 0},{1, 0},{0, -1},{0, 1}};
-	
+
+	static int N, M, maxSafe = 0;
+	static int[][] originMap;
+	static int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		origin = new int[N][M];
-		maxSize = Integer.MIN_VALUE;
-		
-		for(int i = 0; i < N; i++) {
+		originMap = new int[N][M];
+		List<int[]> empty = new ArrayList<>();
+		List<int[]> virus = new ArrayList<>();
+
+		for(int i = 0; i < N; i++){
 			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < M; j++) {
-				origin[i][j] = Integer.parseInt(st.nextToken());
+			for(int j = 0; j < M; j++){
+				originMap[i][j] = Integer.parseInt(st.nextToken());
+				if(originMap[i][j] == 2) virus.add(new int[]{i, j});
+				if(originMap[i][j] == 0) empty.add(new int[]{i, j});
 			}
 		}
-		
-		pickWall(0, 0, new int[3]);
-		System.out.println(maxSize);
-	}
-	
-	private static void pickWall(int cnt, int start, int[] picked) {
-		if(cnt == 3) {
-			map = copyMap();
-			for(int i = 0; i < 3; i++) {
-				int x = picked[i] / M;
-				int y = picked[i] % M;
-				map[x][y] = 1; // 벽 세우기
-			}
 
-			visited = new boolean[N][M];
-			for(int i = 0; i < N; i++) {
-				for(int j = 0; j < M; j++) {
-					if(map[i][j] == 2 && !visited[i][j]) virus(i, j);
+		for(int i = 0; i < empty.size() - 2; i++){
+			for(int j = i + 1; j < empty.size() - 1; j++){
+				for(int k = j + 1; k < empty.size(); k++){
+					int[][] map = initMap();
+					map[empty.get(i)[0]][empty.get(i)[1]] = 1;
+					map[empty.get(j)[0]][empty.get(j)[1]] = 1;
+					map[empty.get(k)[0]][empty.get(k)[1]] = 1;
+					bfs(virus, map);
+					maxSafe = Math.max(maxSafe, getSafeArea(map));
 				}
 			}
+		}
 
-			maxSize = Math.max(checkSafeArea(), maxSize);
-			return;
-		}
-		
-		for(int i = start; i < N * M; i++) {
-			if(origin[i / M][i % M] == 0) {
-				picked[cnt] = i;
-				pickWall(cnt+ 1, i + 1, picked);
-			}
-		}
+		System.out.println(maxSafe);
 	}
-	
-	private static int[][] copyMap() {
-		int[][] copy = new int[N][M];
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < M; j++) {
-				copy[i][j] = origin[i][j];
-			}
+
+	private static int[][] initMap(){
+		int[][] map = new int[N][M];
+		for(int i = 0; i < N; i++){
+			map[i] = originMap[i].clone();
 		}
-		return copy;
+
+		return map;
 	}
-	
-	private static void virus(int x, int y) {
-		Queue<int[]> q = new LinkedList<>();
-		visited[x][y] = true;
-		q.add(new int[] {x, y});
-		
-		while(!q.isEmpty()) {
+
+	private static void bfs(List<int[]> virus, int[][] map){
+		Queue<int[]> q = new LinkedList<>(virus);
+
+		while(!q.isEmpty()){
 			int[] cur = q.poll();
-			x = cur[0];
-			y = cur[1];
-			
-			for(int[] dir : d) {
-				int nx = x + dir[0];
-				int ny = y + dir[1];
-				
-				if(nx < 0 || ny < 0 || nx >= N || ny >= M || visited[nx][ny]) continue;
-				if(map[nx][ny] == 0) {
-					visited[nx][ny] = true;
-					q.add(new int[] {nx, ny});
+			int x = cur[0];
+			int y = cur[1];
+
+			for(int[] d : dir){
+				int nx = x + d[0];
+				int ny = y + d[1];
+
+				if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+				if(map[nx][ny] == 0){
 					map[nx][ny] = 2;
+					q.add(new int[]{nx, ny});
 				}
 			}
 		}
 	}
-	
-	private static int checkSafeArea() {
-		int size = 0;
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < M; j++) {
-				if(map[i][j] == 0) size++;
+
+	private static int getSafeArea(int[][] map){
+		int cnt = 0;
+		for(int i = 0; i < N; i++){
+			for(int j = 0; j < M; j++){
+				if(map[i][j] == 0) cnt++;
 			}
 		}
-		return size;
-		
+
+		return cnt;
 	}
+
 }
