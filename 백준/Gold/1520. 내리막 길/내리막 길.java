d866bf5c0ed71b6a8dbfa18c59fe57acdda787ae
안수진 <88701965+ssuzyn@users.noreply.github.com>
2025-08-08 16:41:45 +0900

    [Gold III] Title: 내리막 길, Time: 344 ms, Memory: 38260 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -2,54 +2,52 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, M;
-    static int[][] map;
-    static int[][] dp;
-    static int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+	static int N, M;
+	static int[][] map;
+	static int[][] dp;
+	static int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken()); // 행
-        N = Integer.parseInt(st.nextToken()); // 열
-        map = new int[M][N];
-        dp = new int[M][N];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken()); // 세로
+		M = Integer.parseInt(st.nextToken()); // 가로
+		map = new int[N][M];
+		dp = new int[N][M];
 
-        for(int i = 0; i < M; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < N; j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+		for(int i = 0; i < N; i++){
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < M; j++){
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+			Arrays.fill(dp[i], -1);
+		}
 
-        for(int i = 0; i < M; i++){
-            Arrays.fill(dp[i], -1);
-        }
+		System.out.println(dfs(0, 0));
+	}
 
-        System.out.println(dfs(0, 0));
-    }
+	private static int dfs(int x, int y){
+		if(dp[x][y] != -1) return dp[x][y];
 
-    private static int dfs(int x, int y){
-        if(x == M-1 && y == N-1) return 1;
+		if(x == N-1 && y == M-1) return 1;
 
-        if(dp[x][y] == -1){
-            dp[x][y] = 0;
+		// 현재 위치에서 갈 수 있는 모든 방향 탐색
+		dp[x][y] = 0;
+		for(int[] d : dir){
+			int nx = x + d[0];
+			int ny = y + d[1];
 
-            for(int i = 0; i < 4; i++){
-                int nx = x + dir[i][0];
-                int ny = y + dir[i][1];
+			if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+			if(map[x][y] > map[nx][ny]) dp[x][y] += dfs(nx, ny);
+		}
 
-                if(nx < 0 || nx >= M || ny < 0 || ny >= N) continue;
-                if(map[x][y] > map[nx][ny]) { // 내리막길인 경우
-                    dp[x][y] += dfs(nx, ny);
-                }
-            }
-        }
-        return dp[x][y];
-    }
-    
+		return dp[x][y];
+	}
+
 }
