dcabc4162ba00bc7f21f7b108e340f6c932119a4
안수진 <88701965+ssuzyn@users.noreply.github.com>
2025-08-09 19:01:50 +0900

    [level 2] Title: 큰 수 만들기, Time: 73.42 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,23 +1,32 @@
-import java.util.Stack;
+import java.util.*;
 
 class Solution {
     public String solution(String number, int k) {
-        Stack<Character> stack = new Stack<>();
-        int keep = number.length() - k;
         
-        for (int i = 0; i < number.length(); i++) {
-            char digit = number.charAt(i);
-            while (!stack.isEmpty() && k > 0 && stack.peek() < digit) {
-                stack.pop();
-                k--;
+        Stack<Character> stack = new Stack<>();
+        int count = 0;
+        
+        for(int i = 0; i < number.length(); i++){
+            char tmp = number.charAt(i);
+            
+            while(count < k && !stack.isEmpty() && stack.peek() < tmp){
+                stack.pop(); // 숫자 제거
+                count++;
             }
-            stack.push(digit);
+            
+            stack.push(tmp);
+        }
+        
+        // k개를 모두 제거하지 못한 경우 뒤에서 제거
+        while(count < k) {
+            stack.pop();
+            count++;
         }
         
         StringBuilder sb = new StringBuilder();
-        while (!stack.isEmpty()) {
-            sb.append(stack.pop());
+        for(Character c : stack){ // stack을 앞에서부터 순서대로 읽음
+            sb.append(c);
         }
-        return sb.reverse().substring(0, keep);
+        return sb.toString();
     }
 }
