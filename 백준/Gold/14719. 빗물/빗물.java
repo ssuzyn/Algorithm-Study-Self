887df3741c012d9bda0b789a307f9636b01b51db
안수진 <88701965+ssuzyn@users.noreply.github.com>
2025-08-13 12:54:15 +0900

    [Gold V] Title: 빗물, Time: 100 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -4,54 +4,48 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int H = Integer.parseInt(st.nextToken());
-		int W = Integer.parseInt(st.nextToken());
-		
-		
-		st = new StringTokenizer(br.readLine());
-		Stack<Integer> block = new Stack<>();
-		int answer = 0;
-		int limit = Integer.parseInt(st.nextToken());
-		
-		while(st.hasMoreTokens()) {
-			int tmp = Integer.parseInt(st.nextToken());
-			
-			if(tmp >= limit) {
-				while(!block.isEmpty()) {
-					answer += (limit - block.pop());
-				}
-				limit = tmp;
+   public static void main(String[] args) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int H = Integer.parseInt(st.nextToken());
+      int W = Integer.parseInt(st.nextToken());
+      Stack<Integer> stack = new Stack<>();
 
-			}
-			else {
-				block.push(tmp);
-			}
-						
-		}
-		
-		
-		if(!block.isEmpty() && block.size() > 1) {
-			int end = block.pop();
-			
-			while(!block.isEmpty()) {
-				int tmp = block.pop();
-				if(tmp < end) {
-					answer += end - tmp;
-				}
-				else {
-					end = tmp;
-				}
-			}
-		}
-		
-		System.out.println(answer);
-		
-	}
+      st = new StringTokenizer(br.readLine());
+      int limit = Integer.parseInt(st.nextToken()); // 왼쪽 벽
+      int totalRain = 0;
 
+      // 1단계: 왼쪽→오른쪽, 높은 벽 만나면 물 계산
+      for(int i = 1; i < W; i++){
+         int tmp = Integer.parseInt(st.nextToken());
+
+         if(tmp < limit) {
+            stack.push(tmp);
+         } else {
+            // 오른쪽 벽 발견! 스택 비우며 물 계산
+            while(!stack.isEmpty()){
+               totalRain += limit - stack.pop();
+            }
+            limit = tmp;
+         }
+      }
+
+      // 2단계: 남은 블록들 오른쪽→왼쪽 처리
+      if(!stack.isEmpty()){
+         int end = stack.pop(); // 오른쪽 벽
+
+         while(!stack.isEmpty()){
+            int tmp = stack.pop();
+            if(tmp < end) {
+               totalRain += end - tmp;
+            } else {
+               end = tmp; // 더 높은 벽 발견
+            }
+         }
+      }
+
+      System.out.println(totalRain);
+   }
 }
