623b21b1419c2aa8bda0574f50a2145c78963ca4
안수진 <88701965+ssuzyn@users.noreply.github.com>
2025-08-14 10:50:25 +0900

    [Silver I] Title: 단지번호붙이기, Time: 104 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,69 +1,63 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.PriorityQueue;
 
 public class Main {
-	
+
+	static int N, count;
+	static boolean[][] map;
 	static int[] dx = {0,0,-1,1};
 	static int[] dy = {-1,1,0,0};
-	static int[][] map;
-	static boolean[][] visited;
-	static int N, count;
-	static List<Integer> houses = new ArrayList<>();
-	
-	private static boolean isValid(int x, int y) {
-		return x >= 0 && x < N && y >= 0 && y < N;
-	}
 
-	private static void dfs(int x, int y) {
-		visited[x][y] = true;
-		
-		for(int i = 0; i < 4; i++) {
-			int newX = x + dx[i];
-			int newY = y + dy[i];
-			
-			if(isValid(newX, newY) && !visited[newX][newY] && map[newX][newY] == 1) {
-				count++;
-				dfs(newX, newY);
-			}
-		}
-		
-	}
-	
 	public static void main(String[] args)throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		map = new int[N][N];
-		visited = new boolean[N][N];
-		
+		map = new boolean[N][N];
+
 		for(int i = 0; i < N; i++) {
 			String input = br.readLine();
 			for(int j = 0; j < N; j++) {
-				map[i][j] = input.charAt(j) - '0';
+				map[i][j] = (input.charAt(j) == '1');
 			}
 		}
-		
+
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
 		for(int i = 0; i < N; i++) {
 			for(int j = 0; j < N; j++) {
-				if(map[i][j] == 1 && !visited[i][j]) {
+				if(map[i][j]) {
 					count = 1;
 					dfs(i, j);
-					houses.add(count);
+					pq.add(count);
 				}
-				
 			}
 		}
-		
-		Collections.sort(houses);
-		
-		System.out.println(houses.size());
-		for(int h : houses) {
-			System.out.println(h);
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(pq.size()).append("\n");
+		while(!pq.isEmpty()) {
+			sb.append(pq.poll()).append("\n");
 		}
-		
+		System.out.print(sb);
+	}
+
+	private static void dfs(int x, int y) {
+		map[x][y] = false;
+
+		for(int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+
+			if(isValid(nx, ny) && map[nx][ny]) {
+				count++;
+				dfs(nx, ny);
+			}
+		}
+
+	}
+
+	private static boolean isValid(int x, int y) {
+		return x >= 0 && x < N && y >= 0 && y < N;
 	}
 
 }
