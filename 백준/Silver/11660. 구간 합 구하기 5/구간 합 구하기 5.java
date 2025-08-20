ef92ac534851ddb395a7cce85405a8deb8d50301
안수진 <88701965+ssuzyn@users.noreply.github.com>
2025-08-20 14:59:33 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 732 ms, Memory: 120004 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -4,31 +4,35 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
+
+	public static void main(String[] args)throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken()); // 표의 크기
-		int M = Integer.parseInt(st.nextToken()); // 합 구하기 횟수
-		
-		int[][]map = new int[N+1][N+1];
-		
-		for(int i = 1; i <= N; i++) { // 표 입력 받기
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		int[][] sum = new int[N + 1][N + 1];
+		for(int i = 1; i <= N; i++){
 			st = new StringTokenizer(br.readLine());
-			for(int j = 1; j <= N; j++) {
+			for(int j = 1; j <= N; j++){
 				int tmp = Integer.parseInt(st.nextToken());
-				map[i][j] = tmp + map[i-1][j] + map[i][j-1] - map[i-1][j-1];
+				sum[i][j] += tmp + sum[i - 1][j] + sum[i][j - 1] - sum[i-1][j-1];
 			}
 		}
-		
-		for(int i = 0; i < M; i++) { // 구해야 하는 합 좌표 받기
+
+		StringBuilder sb = new StringBuilder();
+		while(M-- > 0){
 			st = new StringTokenizer(br.readLine());
 			int x1 = Integer.parseInt(st.nextToken());
 			int y1 = Integer.parseInt(st.nextToken());
 			int x2 = Integer.parseInt(st.nextToken());
 			int y2 = Integer.parseInt(st.nextToken());
-				
-			System.out.println(map[x2][y2] - map[x1-1][y2] - map[x2][y1-1] + map[x1-1][y1-1]);
+
+			int answer = sum[x2][y2] + sum[x1-1][y1-1] - sum[x1-1][y2] - sum[x2][y1-1];
+			sb.append(answer).append("\n");
 		}
+
+		System.out.print(sb);
 	}
+
 }
