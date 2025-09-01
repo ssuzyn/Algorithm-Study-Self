2e05473258b2e743975c9208ee0f223153ef80b6
안수진 <88701965+ssuzyn@users.noreply.github.com>
2025-09-01 16:41:51 +0900

    [Gold IV] Title: 자두나무, Time: 112 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.java
+++ 백준/Gold/2240. 자두나무/자두나무.java
@@ -13,25 +13,22 @@ public class Main {
 		int[] plum = new int[T + 1];
 		int[][][] dp = new int[3][T + 1][W + 2];
 
-		for(int i = 1; i <= T; i++){
-			plum[i] = Integer.parseInt(br.readLine());
-		}
-
 		for(int t = 1; t <= T; t++){
+			plum[t] = Integer.parseInt(br.readLine());
+
 			for(int w = 1; w <= W + 1; w++){
-				if(plum[t] == 1){ // 1번 나무에 있는 경우
+				if(plum[t] == 1){
 					dp[1][t][w] = Math.max(dp[1][t-1][w], dp[2][t-1][w-1]) + 1;
 					dp[2][t][w] = Math.max(dp[2][t-1][w], dp[1][t-1][w-1]);
 				}
-				else{ // 2번 나무에 있는 경우
-					if(t == 1 && w == 1) continue; // 자두는 처음에 1번 나무에 있으므로 1초에 2번 나무로 갈 수 없음
+				else{
+					if(t == 1 && w == 1) continue;
 					dp[1][t][w] = Math.max(dp[1][t-1][w], dp[2][t-1][w-1]);
 					dp[2][t][w] = Math.max(dp[2][t-1][w], dp[1][t-1][w-1]) + 1;
 				}
 			}
 		}
 
-		// T초에 1번 또는 2번 나무에서 가능한 모든 이동횟수 중 최대값
 		int result = 0;
 		for(int w = 1; w <= W + 1; w++){
 			result = Math.max(result, Math.max(dp[1][T][w], dp[2][T][w]));
@@ -39,5 +36,4 @@ public class Main {
 
 		System.out.println(result);
 	}
-
 }
