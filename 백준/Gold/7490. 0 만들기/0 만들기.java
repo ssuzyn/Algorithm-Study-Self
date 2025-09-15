8c9ee43d3e2319a46e143c69bf78d01eabec1880
안수진 <88701965+ssuzyn@users.noreply.github.com>
2025-09-15 23:57:19 +0900

    [Gold V] Title: 0 만들기, Time: 152 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.java
+++ 백준/Gold/7490. 0 만들기/0 만들기.java
@@ -1,62 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
 
 public class Main {
 
 	static int N;
-	static String[] op = {"+", "-", " "};
-	static List<String> answer;
+	static StringBuilder sb = new StringBuilder();
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
 		while(T-- > 0){
 			N = Integer.parseInt(br.readLine());
-			answer = new ArrayList<>();
-			makeZero(1, "1");
-
-			Collections.sort(answer);
-			for(String s : answer){
-				sb.append(s).append("\n");
-			}
+			makeZero(1, 1, 0, 1, "1");
 			sb.append("\n");
 		}
 
 		System.out.print(sb);
-
 	}
 
-	private static void makeZero(int cur, String str){
-		if(cur == N){
-			String express = str.replaceAll(" ", "");
-			if(isZero(express)){
-				answer.add(str);
-			}
+	private static void makeZero(int cur, int num, int result, int op, String express) {
+		if (cur == N) {
+			result += (num * op);
+			if(result == 0) sb.append(express).append("\n");
 			return;
 		}
 
-		for(int i = 0; i < 3; i++){
-			makeZero(cur + 1, str + op[i] + Integer.toString(cur + 1));
-		}
-	}
-
-	private static boolean isZero(String express){
-		StringTokenizer st = new StringTokenizer(express, "-|+", true);
-		int sum = Integer.parseInt(st.nextToken());
-
-		while(st.hasMoreTokens()){
-			String s = st.nextToken();
-			if(s.equals("+")) sum += Integer.parseInt(st.nextToken());
-			else sum -= Integer.parseInt(st.nextToken());
-		}
-
-		return sum == 0;
+		makeZero(cur + 1, num * 10 + (cur + 1), result, op, express + " " + Integer.toString(cur + 1));
+		makeZero(cur + 1, cur + 1, result + num * op, 1, express + "+" + Integer.toString(cur + 1));
+		makeZero(cur + 1, cur + 1, result + num * op, -1, express + "-" + Integer.toString(cur + 1));
 	}
 
 }
