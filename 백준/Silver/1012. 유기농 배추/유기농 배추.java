c4d8ae602b68337ccf551c8b6600135e09d6249e
안수진 <88701965+ssuzyn@users.noreply.github.com>
2025-09-18 13:52:20 +0900

    [Silver II] Title: 유기농 배추, Time: 156 ms, Memory: 16364 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,70 +1,73 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int[][] field;
+	static int N, M;
+	static int[][] map;
 	static boolean[][] visited;
-	static int[] dx = {0, 0, -1, 1};
-	static int[] dy = {-1, 1, 0, 0};
-	static int M, N;
-	
-	private static boolean isValid(int x, int y) {
-		return x >= 0 && y >= 0 && x < M && y < N;
-	}
-	
-	private static void dfs(int x, int y) {
-		
-		for(int i = 0; i < 4; i++) {
-			int newX = x + dx[i];
-			int newY = y + dy[i];
-			
-			if(isValid(newX, newY) && !visited[newX][newY] && field[newX][newY] == 1) {
-				visited[newX][newY] = true;
-				dfs(newX, newY);
-			}
-		}
-	}
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
-		StringTokenizer st;
-		
-		for(int t = 0; t < T; t++) {
-			st = new StringTokenizer(br.readLine());
-			
-			M = Integer.parseInt(st.nextToken());
+		StringBuilder sb = new StringBuilder();
+
+		while(T-- > 0){
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
-			int K = Integer.parseInt(st.nextToken());
-			
-			field = new int[M][N];
-			visited = new boolean[M][N];
-			for(int i = 0; i < K; i++) {
+			M = Integer.parseInt(st.nextToken());
+			int K = Integer.parseInt(st.nextToken()); // 배추 개수
+
+			map = new int[N][M];
+			visited = new boolean[N][M];
+			List<int[]> cabbage = new ArrayList<>();
+			for(int i = 0; i < K; i++){
 				st = new StringTokenizer(br.readLine());
 				int x = Integer.parseInt(st.nextToken());
 				int y = Integer.parseInt(st.nextToken());
-				field[x][y] = 1;
+				map[x][y] = 1;
+				cabbage.add(new int[]{x, y});
 			}
-			
-			int earthWorm = 0;
-			for(int i = 0; i < M; i++) {
-				for(int j = 0; j < N; j++) {
-					if(!visited[i][j] && field[i][j] == 1) {
-						dfs(i, j);
-						earthWorm++;
-					}
-				}
-			}
-			
-			System.out.println(earthWorm);
-		}
-		
-		
-		
 
+			int count = 0;
+			for(int[] c : cabbage){
+				int x = c[0], y = c[1];
+				if(visited[x][y]) continue;
+				bfs(x, y);
+				count++;
+			}
+
+			sb.append(count).append("\n");
+		}
+
+		System.out.print(sb);
 	}
 
+	private static void bfs(int x, int y){
+		int[][] dir = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
+		Queue<int[]> q = new LinkedList<>();
+		q.add(new int[]{x, y});
+		visited[x][y] = true;
+
+		while(!q.isEmpty()){
+			int[] cur = q.poll();
+			x = cur[0];
+			y = cur[1];
+
+			for(int[] d : dir){
+				int nx = x + d[0];
+				int ny = y + d[1];
+
+				if(nx < 0 || ny < 0 || nx >= N || ny >= M || visited[nx][ny] || map[nx][ny] == 0) continue;
+				visited[nx][ny] = true;
+				q.add(new int[]{nx, ny});
+			}
+		}
+	}
 }
