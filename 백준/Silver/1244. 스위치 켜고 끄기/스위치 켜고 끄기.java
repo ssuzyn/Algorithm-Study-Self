3a89efee56c287d5aea9abebc86820c7c5ee5f97
안수진 <88701965+ssuzyn@users.noreply.github.com>
2025-09-20 18:54:27 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 120 ms, Memory: 14580 KB -BaekjoonHub

---
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N;
+	static int[] light;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		light = new int[N + 1];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 1; i <= N; i++){
+			light[i] = Integer.parseInt(st.nextToken());
+		}
+
+		int M = Integer.parseInt(br.readLine());
+		while(M-- > 0){
+			st = new StringTokenizer(br.readLine());
+			int gender = Integer.parseInt(st.nextToken());
+			int num = Integer.parseInt(st.nextToken());
+
+			if(gender == 1) switchByMale(num);
+			else switchByFemale(num);
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for(int i = 1; i <= N; i++){
+			sb.append(light[i] + " ");
+
+			if(i % 20 == 0) {
+				sb.append("\n");
+			}
+		}
+		System.out.println(sb);
+	}
+
+	private static void switchByMale(int num){
+		for(int i = num; i <= N; i+=num){
+			if(light[i] == 0) light[i] = 1;
+			else light[i] = 0;
+		}
+	}
+
+	private static void switchByFemale(int num){
+		int left = num - 1;
+		int right = num + 1;
+		while(1 <= left && right <= N){
+			if(light[left] == light[right]){
+				left--;
+				right++;
+			}
+			else break;
+		}
+
+		for(int i = left + 1; i < right; i++){
+			if(light[i] == 0) light[i] = 1;
+			else light[i] = 0;
+		}
+	}
+
+}
