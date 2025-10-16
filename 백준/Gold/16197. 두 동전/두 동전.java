7576b68087760d1404f1a1dc7f711f674c612afb
안수진 <88701965+ssuzyn@users.noreply.github.com>
2025-10-16 23:19:41 +0900

    [Gold IV] Title: 두 동전, Time: 108 ms, Memory: 14564 KB -BaekjoonHub

--- 백준/Gold/16197. 두 동전/두 동전.java
+++ 백준/Gold/16197. 두 동전/두 동전.java
@@ -1,85 +1,85 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	static int N, M, answer;
+	static int N, M;
 	static char[][] map;
-	static int[][] dir = {{-1, 0},{1, 0},{0, -1},{0, 1}};
+	static int[][] dir = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+	static boolean[][][][] visited;
 
-	public static void main(String[] args)throws IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken()); // 세로 크기
-		M = Integer.parseInt(st.nextToken()); // 가로 크기
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		map = new char[N][M];
 
+		int[][] coins = new int[2][2];
 		int idx = 0;
-		int[][] coin = new int[2][2];
-		for(int i = 0; i < N; i++) {
-			map[i] = br.readLine().toCharArray();
-			for(int j = 0; j < M; j++){
-				if(map[i][j] == 'o') {
-					coin[idx++] = new int[]{i, j};
+
+		for (int i = 0; i < N; i++) {
+			char[] input = br.readLine().toCharArray();
+			for (int j = 0; j < M; j++) {
+				map[i][j] = input[j];
+				if (map[i][j] == 'o') {
+					coins[idx][0] = i;
+					coins[idx][1] = j;
+					idx++;
 					map[i][j] = '.';
 				}
 			}
 		}
 
-		answer = Integer.MAX_VALUE;
-		dfs(coin[0][0], coin[0][1], coin[1][0], coin[1][1], 0);
-		System.out.println(answer == Integer.MAX_VALUE? -1 : answer);
+		int result = bfs(coins[0][0], coins[0][1], coins[1][0], coins[1][1]);
+		System.out.println(result);
 	}
 
-	private static void dfs(int x1, int y1, int x2, int y2, int cnt){
-		if(cnt > 10) return;
+	private static int bfs(int x1, int y1, int x2, int y2) {
+		Queue<int[]> q = new LinkedList<>();
+		visited = new boolean[N][M][N][M];
+		q.add(new int[]{x1, y1, x2, y2, 0});
+		visited[x1][y1][x2][y2] = true;
 
-		boolean coin1Out = !isValid(x1, y1);
-		boolean coin2Out = !isValid(x2, y2);
+		while (!q.isEmpty()) {
+			int[] cur = q.poll();
+			int cx1 = cur[0], cy1 = cur[1], cx2 = cur[2], cy2 = cur[3], cnt = cur[4];
 
-		// 둘 다 떨어짐 - 실패
-		if(coin1Out && coin2Out) {
-			return;
-		}
+			if (cnt >= 10) continue;
 
-		// 하나만 떨어짐 - 성공
-		if(coin1Out || coin2Out) {
-			answer = Math.min(answer, cnt);
-			return;
-		}
+			for (int[] d : dir) {
+				int nx1 = cx1 + d[0];
+				int ny1 = cy1 + d[1];
+				int nx2 = cx2 + d[0];
+				int ny2 = cy2 + d[1];
 
-		for(int[] d : dir){
-			int nx1 = x1 + d[0];
-			int nx2 = x2 + d[0];
-			int ny1 = y1 + d[1];
-			int ny2 = y2 + d[1];
+				boolean fall1 = !isValid(nx1, ny1);
+				boolean fall2 = !isValid(nx2, ny2);
 
-			if(!canGo(nx1, ny1)) {
-				nx1 = x1;
-				ny1 = y1;
+				if (fall1 && fall2) continue; // 둘 다 떨어짐 → 실패
+				if (fall1 ^ fall2) return cnt + 1; // 하나만 떨어짐 → 성공
+
+				// 벽에 부딪히면 이동하지 않음
+				if (!fall1 && map[nx1][ny1] == '#') {
+					nx1 = cx1;
+					ny1 = cy1;
+				}
+				if (!fall2 && map[nx2][ny2] == '#') {
+					nx2 = cx2;
+					ny2 = cy2;
+				}
+
+				if (!visited[nx1][ny1][nx2][ny2]) {
+					visited[nx1][ny1][nx2][ny2] = true;
+					q.add(new int[]{nx1, ny1, nx2, ny2, cnt + 1});
+				}
 			}
-			if(!canGo(nx2, ny2)){
-				nx2 = x2;
-				ny2 = y2;
-			}
-
-			dfs(nx1, ny1, nx2, ny2, cnt + 1);
 		}
 
+		return -1; // 10번 이내에 불가능
 	}
 
-	private static boolean canGo(int x, int y){
-		// 보드 안에 있으면서 벽인 경우에만 이동 불가
-		if(isValid(x, y) && map[x][y] == '#') {
-			return false;
-		}
-		return true;
+	private static boolean isValid(int x, int y) {
+		return 0 <= x && x < N && 0 <= y && y < M;
 	}
-
-	private static boolean isValid(int x, int y){
-		return x >= 0 && y >= 0 && x < N && y < M;
-	}
-
 }
